209c1400868ff05dcbd667134162bc1d7dbf0b80
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-05-17 13:40:56 +0200

    Updated module-create command

--- scaffold-app.sh
+++ scaffold-app.sh
@@ -4,7 +4,7 @@ set -ex
 
 # Login
 yarn nx generate @nrwl/angular:component login --project=app-login-feature --export --flat --standalone --type=page # use --type=page if the component is a page else use --type=component if it is a small component on a page e.g. card component
-yarn nx generate @nrwl/angular:module login --project=app-login-feature --routing
+yarn nx generate @nrwl/angular:module login --project=app-login-feature --routing # this creates a module (login.module.ts) with routing (login-routing.module.ts) but you must rename the rooting module to login.routing.ts
 
 # ============================================================================================================================
 
