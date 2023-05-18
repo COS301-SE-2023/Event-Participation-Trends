ea8297e8b9538349317039881d6c120ae548cd82
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-05-18 08:41:29 +0200

    Added routing for login page

--- libs/app/core/feature/src/core.routing.ts
+++ libs/app/core/feature/src/core.routing.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    canActivate: [],
+    data: {},
+    loadChildren: () =>
+      import('@event-participation-trends/app/login/feature').then((m) => m.LoginModule),
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes,  {preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class CoreRoutingModule { }
