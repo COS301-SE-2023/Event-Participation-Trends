9c1897f6eca98737324e1fc911c83ac5584d9ff8
Stefan van der Merwe <u21429121@tuks.co.za>
2023-05-18 19:35:08 +0200

    Add MQTT

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
-
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MqttController } from './mqtt.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ClientsModule.register([
+      {
+        name: "MQTT_SERVICE",
+        transport: Transport.MQTT,
+        options: {
+          url: process.env.MQTT_URL,
+          username: process.env.MQTT_USERNAME,
+          password: process.env.MQTT_PASSWORD,
+        }
+      }
+    ]),
+  ],
+  controllers: [AppController, MqttController],
   providers: [AppService],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,10 +5,12 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { Transport, MicroserviceOptions } from '@nestjs/microservices'
 import { AppModule } from './app/app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
@@ -17,6 +19,17 @@ async function bootstrap() {
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+
+  const mqtt_app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.MQTT,
+    options: {
+      url: process.env.MQTT_URL,
+      username: process.env.MQTT_USERNAME,
+      password: process.env.MQTT_PASSWORD,
+    },
+  });
+  mqtt_app.listen();
+  Logger.log("💥 MQTT Microservice is listening");
 }
 
 bootstrap();
