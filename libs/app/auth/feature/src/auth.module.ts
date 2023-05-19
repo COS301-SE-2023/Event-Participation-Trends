294146460d148880384af704cf454c198497bb53
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-05-19 12:02:24 +0200

    Added code for auth.module in auth/feature

---
+++ libs/app/auth/feature/src/auth.module.ts
@@ -0,0 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AuthModule as AuthDataAccessModule } from '@event-participation-trends/app/auth/data-access';
+
+@NgModule({
+  imports: [CommonModule, AuthDataAccessModule],
+})
+export class AuthModule {}
