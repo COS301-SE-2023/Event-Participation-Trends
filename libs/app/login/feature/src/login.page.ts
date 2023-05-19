abb3692c8df929bf8c243541304a6c21a44e2ed8
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-05-19 12:00:08 +0200

    Added state management for app/login

--- libs/app/login/feature/src/login.page.ts
+++ libs/app/login/feature/src/login.page.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { ContinueWithGoogle } from '@event-participation-trends/app/login/util';
 
 @Component({
   selector: 'event-participation-trends-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.css'],
 })
-export class LoginPage {}
+export class LoginPage {
+  // constructor(private readonly store: Store) {}
+
+  // continueWithGoogle() {
+  //   this.store.dispatch(new ContinueWithGoogle());
+  // }
+}
