d72d5331541daf4d11205e065f8fbac6ea1e303a
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-05-19 12:08:05 +0200

    Generated files for app/error

--- scaffold-app.sh
+++ scaffold-app.sh
@@ -11,12 +11,17 @@ set -ex
 yarn nx generate @nrwl/angular:component core --project=app-core-feature --export --flat --type=shell
 yarn nx generate @nrwl/angular:module core --project=app-core-feature --routing
 
+# ERROR
+yarn nx generate @nrwl/angular:module error --project=app-error-feature
+yarn nx generate @nrwl/angular:module error --project=app-error-data-access
+
 # LOGIN
 yarn nx generate @nrwl/angular:component login --project=app-login-feature --export --flat --type=page
 yarn nx generate @nrwl/angular:module login --project=app-login-feature --routing
 
 # AUTH
 yarn nx generate @nrwl/angular:module auth --project=app-auth-feature
+yarn nx generate @nrwl/angular:module auth --project=app-auth-data-access
 
 # ============================================================================================================================
 
@@ -24,6 +29,11 @@ yarn nx generate @nrwl/angular:module auth --project=app-auth-feature
 
 # CORE
 yarn nx generate @nrwl/js:library feature --unitTestRunner=jest --directory=app/core --no-interactive
+
+# ERROR
+yarn nx generate @nrwl/js:library feature --unitTestRunner=jest --directory=app/error --no-interactive
+yarn nx generate @nrwl/js:library data-access --unitTestRunner=jest --directory=app/error --no-interactive
+yarn nx generate @nrwl/js:library util --unitTestRunner=jest --directory=app/error --no-interactive
 
 # LOGIN
 yarn nx generate @nrwl/js:library feature --unitTestRunner=jest --directory=app/login --no-interactive     # --> feature
