537e4642db017823697f2e2e7f96dfdcb4a0de60
Stefan van der Merwe <u21429121@tuks.co.za>
2023-05-20 15:30:30 +0200

    Remove sensitive info

--- libs/passport/src/lib/passport.service.ts
+++ libs/passport/src/lib/passport.service.ts
@@ -1,3 +1,4 @@
+import { UntypedFormBuilder } from '@angular/forms';
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
@@ -5,7 +6,15 @@ import { JwtService } from '@nestjs/jwt';
 export class PassportService {
     constructor(private readonly jwtService: JwtService) {}
     async generateJWT(toSign: any){
-        console.log(toSign.user);
-        return await this.jwtService.signAsync(toSign.user);
+        const user = toSign.user;
+        if(user === undefined)
+            return "";
+        const willSign = {
+            email: user.email,
+            firstName: user.firstName,
+            lastnName: user.lastName,
+            picture: user.picture,
+        };
+        return await this.jwtService.signAsync(willSign);
     }
 }
