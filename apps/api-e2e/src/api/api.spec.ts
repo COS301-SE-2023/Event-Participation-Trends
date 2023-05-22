a48f6b7a01b83cc8b1499067be775cccdab9f268
Stefan van der Merwe <u21429121@tuks.co.za>
2023-05-22 10:15:25 +0200

    Add beforeAll to setup api for testing

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -1,14 +1,39 @@
 import axios from 'axios';
+import { spawn } from 'child_process';
 
 describe('GET /api', () => {
+  let apiServer;
+  beforeAll(async () => {
+    axios.defaults.baseURL = 'http://localhost:3000';
+    apiServer = spawn('yarn', ['start:dev'], {
+      cwd: './',
+      shell: true,
+    });
+    let notUp = true;
+    while (notUp) {
+      try {
+        await axios.get('api/');
+      } catch (error) {
+        console.log(error);
+        if (error?.response?.status === 401) {
+          notUp = false;
+        }
+      }
+    }
+  }, 10000);
+
   let res;
   it('should return a message', async () => {
     try {
       res = await axios.get('api/');
       console.log(res);
     } catch (error) {
-      expect(error.response.status).toEqual(401); // Since we are not logged in
+      expect(error.response.status).toEqual(401);
       return;
     }
+  });
+
+  afterAll(() => {
+    apiServer.kill();
   });
 });
