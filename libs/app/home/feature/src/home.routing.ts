f41f74bd885d357381ed6c3270905ecb711b8524
Arno <u21457451@tuks.co.za>
2023-05-23 09:40:50 +0200

    Added default route

--- libs/app/home/feature/src/home.routing.ts
+++ libs/app/home/feature/src/home.routing.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {    
+    path: '',
+    component: HomePage,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home', //this is only temporary
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
