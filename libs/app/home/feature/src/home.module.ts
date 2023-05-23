d092af622e26b43747f914656bc2f43b3b718321
Arno <u21457451@tuks.co.za>
2023-05-23 08:58:23 +0200

    Renamed and moved files to their correct path

--- libs/app/home/feature/src/home.module.ts
+++ libs/app/home/feature/src/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
+import { HomeRoutingModule } from './home.routing';
 
 
 @NgModule({
