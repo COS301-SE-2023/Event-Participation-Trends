c47b3071893c27e01360c81215105d39f0792b84
Arno <u21457451@tuks.co.za>
2023-05-23 09:09:15 +0200

    Fixed HomePage

--- libs/app/home/feature/src/home.page.ts
+++ libs/app/home/feature/src/home.page.ts
@@ -1,1 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ms-home-page',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.css'],
+  })
 export class HomePage {}
