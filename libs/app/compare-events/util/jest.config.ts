585521157cc32ee255b1e1b61a3ac7819c7a3534
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-05-29 11:30:24 +0200

    Generated library files for compare-events

---
+++ libs/app/compare-events/util/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'app-compare-events-util',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/app/compare-events/util',
+};
