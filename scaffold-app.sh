eb2f10fb644c185904deb21bb599b2af5898c931
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-05-29 11:08:45 +0200

    Added commands for the view-events page

--- scaffold-app.sh
+++ scaffold-app.sh
@@ -8,7 +8,7 @@ set -ex
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # CORE
-yarn nx generate @nrwl/angular:component core --project=app-core-feature --export --flat --type=shell
+yarn nx generate @nrwl/angular:component core --project=app-core-feature --export --flat --type=shell --standalone
 yarn nx generate @nrwl/angular:module core --project=app-core-feature --routing
 
 # ERROR
@@ -27,8 +27,14 @@ yarn nx generate @nrwl/angular:module auth --project=app-auth-data-access
 yarn nx generate @nrwl/angular:module home --project=app-home-feature --routing
 
 # USER MANAGEMENT (Tab visible for Admins only)
-yarn nx generate @nrwl/angular:component user-management --project=app-user-management-feature --export --flat --type=page
+yarn nx generate @nrwl/angular:component user-management --project=app-user-management-feature --export --flat --type=page --standalone
 yarn nx generate @nrwl/angular:module user-management --project=app-user-management-feature --routing
+yarn nx generate @nrwl/angular:module user-management --project=app-user-management-data-access
+
+# VIEW EVENTS
+yarn nx generate @nrwl/angular:component view-events --project=app-view-events-feature --export --flat --type=page --standalone
+yarn nx generate @nrwl/angular:module view-events --project=app-view-events-feature --routing
+yarn nx generate @nrwl/angular:module view-events --project=app-view-events-data-access
 
 # ============================================================================================================================
 
@@ -59,3 +65,8 @@ yarn nx generate @nrwl/js:library feature --unitTestRunner=jest --directory=app/
 yarn nx generate @nrwl/js:library feature --unitTestRunner=jest --directory=app/user-management --no-interactive     # --> feature
 yarn nx generate @nrwl/js:library data-access --unitTestRunner=jest --directory=app/user-management --no-interactive # --> data-access
 yarn nx generate @nrwl/js:library util --unitTestRunner=jest --directory=app/user-management --no-interactive        # --> util
+
+# VIEW EVENTS
+yarn nx generate @nrwl/js:library feature --unitTestRunner=jest --directory=app/view-events --no-interactive     # --> feature
+yarn nx generate @nrwl/js:library data-access --unitTestRunner=jest --directory=app/view-events --no-interactive # --> data-access
+yarn nx generate @nrwl/js:library util --unitTestRunner=jest --directory=app/view-events --no-interactive        # --> util
