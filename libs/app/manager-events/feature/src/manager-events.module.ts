132e7c2cf346e18165f2d778a54271701f6bf0c6
Arno <u21457451@tuks.co.za>
2023-05-30 17:06:06 +0200

    Moved files to src

--- libs/app/manager-events/feature/src/manager-events.module.ts
+++ libs/app/manager-events/feature/src/manager-events.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ManagerEventsRoutingModule } from './manager-events-routing.module';
+import { ManagerEventsRoutingModule } from './manager-events.routing';
 
 
 @NgModule({
