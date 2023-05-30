1227fe1ab0372c117146a11bc23ce6665c293863
Arno <u21457451@tuks.co.za>
2023-05-30 17:15:34 +0200

    Added state template

--- libs/app/manager-events/data-access/src/manager-events.module.ts
+++ libs/app/manager-events/data-access/src/manager-events.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgxsModule } from '@ngxs/store';
+import { ManagerEventsState } from './manager-events.state';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, NgxsModule.forFeature([ManagerEventsState])]
 })
 export class ManagerEventsModule { }
