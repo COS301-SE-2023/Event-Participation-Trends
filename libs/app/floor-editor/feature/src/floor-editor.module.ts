17446882b7e6c15548e77fdbb66806a37b9d38f7
Arno <u21457451@tuks.co.za>
2023-05-30 17:26:56 +0200

    Renamed routing file

--- libs/app/floor-editor/feature/src/floor-editor.module.ts
+++ libs/app/floor-editor/feature/src/floor-editor.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FloorEditorRoutingModule } from './floor-editor-routing.module';
+import { FloorEditorRoutingModule } from './floor-editor.routing';
 
 
 @NgModule({
