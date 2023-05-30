363714268227450afe3f31ce51fe4132609e1850
Arno <u21457451@tuks.co.za>
2023-05-30 17:30:58 +0200

    Corrected comment

--- libs/app/manager-events/data-access/src/manager-events.state.ts
+++ libs/app/manager-events/data-access/src/manager-events.state.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Action, State } from '@ngxs/store';
 
-// Once we know the interface for the dashboard page we can remove the comment from the line below
+// Once we know the interface for the "My Events" page we can remove the comment from the line below
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ManagerEventsStateModel {}
 
