9c541c0bde2562e177908e30d666031e2d740e8e
Stefan van der Merwe <u21429121@tuks.co.za>
2023-05-31 00:06:43 +0200

    Remove MQTT_TOPIC env uses

--- apps/api/src/app/mqtt.controller.ts
+++ apps/api/src/app/mqtt.controller.ts
@@ -6,11 +6,11 @@ export class MqttController {
     @MessagePattern('/ble')
     async mqtt_ble(@Payload() data) {
         console.log(data);
-        return `Received ${data} on ${process.env.MQTT_TOPIC_BLE}}`
+        return `Received ${data} on /ble`
     };
     @MessagePattern('/wifi')
     async mqtt_wifi(@Payload() data) {
         console.log(data);
-        return `Received ${data} on ${process.env.MQTT_TOPIC_WIFI}}`
+        return `Received ${data} on /wifi`
     }
 }
