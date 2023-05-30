42877eaf670c64a59a941657bba50da05b44779c
Stefan van der Merwe <u21429121@tuks.co.za>
2023-05-31 00:05:38 +0200

    Fix TypeError on deployment environment

--- apps/api/src/app/mqtt.controller.ts
+++ apps/api/src/app/mqtt.controller.ts
@@ -3,12 +3,12 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('mqtt')
 export class MqttController {
-    @MessagePattern(process.env.MQTT_TOPIC_BLE)
+    @MessagePattern('/ble')
     async mqtt_ble(@Payload() data) {
         console.log(data);
         return `Received ${data} on ${process.env.MQTT_TOPIC_BLE}}`
     };
-    @MessagePattern(process.env.MQTT_TOPIC_WIFI)
+    @MessagePattern('/wifi')
     async mqtt_wifi(@Payload() data) {
         console.log(data);
         return `Received ${data} on ${process.env.MQTT_TOPIC_WIFI}}`
