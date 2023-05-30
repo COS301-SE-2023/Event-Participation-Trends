1e2d99a81b2c2f75853274eb327d685226dc48bc
Arno <u21457451@tuks.co.za>
2023-05-30 17:41:22 +0200

    Renamed and updated routing file

--- libs/app/event-details/feature/src/event-details.module.ts
+++ libs/app/event-details/feature/src/event-details.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { EventDetailsRoutingModule } from './event-details-routing.module';
+import { EventDetailsRoutingModule } from './event-details.routing';
 
 
 @NgModule({
--- libs/app/event-details/feature/src/event-details.routing.ts
+++ libs/app/event-details/feature/src/event-details.routing.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventDetailsPage } from './event-details.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: EventDetailsPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
