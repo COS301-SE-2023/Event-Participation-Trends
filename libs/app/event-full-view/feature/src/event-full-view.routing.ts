fd091ad3184b8e1ac39efaf07a989ad8a9c31c04
Arno <u21457451@tuks.co.za>
2023-05-30 17:57:28 +0200

    Added route

--- libs/app/event-full-view/feature/src/event-full-view.routing.ts
+++ libs/app/event-full-view/feature/src/event-full-view.routing.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: EventFullViewPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- libs/app/manager-events/feature/src/manager-events.routing.ts
+++ libs/app/manager-events/feature/src/manager-events.routing.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ManagerEventsPage } from './manager-events.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ManagerEventsPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
