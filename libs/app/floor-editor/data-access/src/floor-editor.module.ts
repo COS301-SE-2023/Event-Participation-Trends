11eeba67bc9dc7080a43921f5c6a387129374d0f
Arno <u21457451@tuks.co.za>
2023-05-30 17:34:27 +0200

    Fixed errors

--- libs/app/floor-editor/data-access/src/floor-editor.module.ts
+++ libs/app/floor-editor/data-access/src/floor-editor.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FloorEditorState } from './floor-editor.state';
+import { NgxsModule } from '@ngxs/store';
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    CommonModule, NgxsModule.forFeature([FloorEditorState])
   ]
 })
 export class FloorEditorModule { }
