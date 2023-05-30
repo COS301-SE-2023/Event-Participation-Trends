a15450d2067c8c462b48a8d0281d2167b808aa5b
Arno <u21457451@tuks.co.za>
2023-05-30 17:38:09 +0200

    Addded component for event details

--- libs/app/event-details/feature/src/index.ts
+++ libs/app/event-details/feature/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/app-event-details-feature';
+
+export * from './lib/event-details.page';
---
+++ libs/app/event-details/feature/src/lib/event-details.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'event-participation-trends-event-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './event-details.page.html',
+  styleUrls: ['./event-details.page.css'],
+})
+export class EventDetailsPage {}
