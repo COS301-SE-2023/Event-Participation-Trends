0b1bfe5ddb03ffcf1b5833ba3669658cc5d321fb
Arno <u21457451@tuks.co.za>
2023-05-30 17:32:53 +0200

    Added route

--- libs/app/floor-editor/feature/src/floor-editor.routing.ts
+++ libs/app/floor-editor/feature/src/floor-editor.routing.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FloorEditorPage } from './floor-editor.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: FloorEditorPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
