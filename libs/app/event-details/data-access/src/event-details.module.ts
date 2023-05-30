018225aa8eceb8686176b0d5982a9a1933f9184c
Arno <u21457451@tuks.co.za>
2023-05-30 17:46:00 +0200

    Fixed import

--- libs/app/event-details/data-access/src/event-details.module.ts
+++ libs/app/event-details/data-access/src/event-details.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgxsModule } from '@ngxs/store';
+import { EventDetailsState } from './event-details.state';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, NgxsModule.forFeature([EventDetailsState])
   ]
 })
 export class EventDetailsModule { }
