fb3cf20d7a40e93c5489678fa7a8394fa3ec11bd
Arno <u21457451@tuks.co.za>
2023-05-30 18:03:31 +0200

    Fixed error

--- libs/app/event-full-view/data-access/src/event-full-view.module.ts
+++ libs/app/event-full-view/data-access/src/event-full-view.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgxsModule } from '@ngxs/store';
+import { EventFullViewState } from './event-full-view.state';
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    CommonModule, NgxsModule.forFeature([EventFullViewState])
   ]
 })
 export class EventFullViewModule { }
