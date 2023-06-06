4a6edb2719a34081aa5197d2ba32f9acb8e7b222
Arno <u21457451@tuks.co.za>
2023-06-06 16:36:02 +0200

    Added prettier formatting

--- libs/app/comparingevents/feature/src/comparingevents.page.ts
+++ libs/app/comparingevents/feature/src/comparingevents.page.ts
@@ -7,7 +7,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class ComparingeventsPage {
   @ViewChild('content-body', { static: true }) contentBody!: ElementRef;
-  
+
   selectedEvents: any[] = [];
 
   constructor(private containerElement: ElementRef) {}
@@ -22,7 +22,6 @@ export class ComparingeventsPage {
     this.overflow = container.scrollHeight > container.clientHeight;
   }
 
-
   // define events array
   events = [
     {
@@ -31,7 +30,7 @@ export class ComparingeventsPage {
       date: '2021-01-01',
       location: 'Location 1',
       description: 'Description 1',
-      selected: false
+      selected: false,
     },
     {
       id: 2,
@@ -39,7 +38,7 @@ export class ComparingeventsPage {
       date: '2021-01-02',
       location: 'Location 2',
       description: 'Description 2',
-      selected: false
+      selected: false,
     },
     {
       id: 3,
@@ -47,7 +46,7 @@ export class ComparingeventsPage {
       date: '2021-01-03',
       location: 'Location 3',
       description: 'Description 3',
-      selected: false
+      selected: false,
     },
     {
       id: 4,
@@ -55,7 +54,7 @@ export class ComparingeventsPage {
       date: '2021-01-04',
       location: 'Location 4',
       description: 'Description 4',
-      selected: false
+      selected: false,
     },
     {
       id: 5,
@@ -63,7 +62,7 @@ export class ComparingeventsPage {
       date: '2021-01-01',
       location: 'Location 5',
       description: 'Description 5',
-      selected: false
+      selected: false,
     },
     {
       id: 6,
@@ -71,7 +70,7 @@ export class ComparingeventsPage {
       date: '2021-01-02',
       location: 'Location 6',
       description: 'Description 6',
-      selected: false
+      selected: false,
     },
     {
       id: 7,
@@ -79,7 +78,7 @@ export class ComparingeventsPage {
       date: '2021-01-03',
       location: 'Location 7',
       description: 'Description 7',
-      selected: false
+      selected: false,
     },
     {
       id: 8,
@@ -87,42 +86,41 @@ export class ComparingeventsPage {
       date: '2021-01-04',
       location: 'Location 8',
       description: 'Description 8',
-      selected: false
+      selected: false,
     },
-];
+  ];
 
-maxSelectionReached = false;
+  maxSelectionReached = false;
 
-toggleItemSelection(event: any) {
-  if (this.selectedEvents.includes(event)) {
-    // Remove from selectedEvents
-    this.selectedEvents = this.selectedEvents.filter(selectedEvent => selectedEvent !== event);
-    event.selected = false;
-  } else if (this.selectedEvents.length < 2) {
-    // Add to selectedEvents
-    event.selected = true;
-    this.selectedEvents.push(event);
+  toggleItemSelection(event: any) {
+    if (this.selectedEvents.includes(event)) {
+      // Remove from selectedEvents
+      this.selectedEvents = this.selectedEvents.filter(
+        (selectedEvent) => selectedEvent !== event
+      );
+      event.selected = false;
+    } else if (this.selectedEvents.length < 2) {
+      // Add to selectedEvents
+      event.selected = true;
+      this.selectedEvents.push(event);
+    }
+
+    if (this.selectedEvents.length < 2) {
+      this.maxSelectionReached = false;
+    } else {
+      this.maxSelectionReached = true;
+    }
   }
 
-  if (this.selectedEvents.length < 2) {
-    this.maxSelectionReached = false;
+  isItemDisabled(event: any): boolean {
+    return this.isMaxSelectionReached() && !this.isSelected(event);
   }
-  else {
-    this.maxSelectionReached = true;
+
+  isMaxSelectionReached(): boolean {
+    return this.maxSelectionReached && this.selectedEvents.length >= 2;
   }
-}
 
-isItemDisabled(event: any): boolean {
-  return this.isMaxSelectionReached() && !this.isSelected(event);
-}
-
-isMaxSelectionReached(): boolean {
-  return this.maxSelectionReached && this.selectedEvents.length >= 2;
-}
-
-
-isSelected(event: any): boolean {
-  return event.selected;
-}
-  
+  isSelected(event: any): boolean {
+    return event.selected;
+  }
 }
