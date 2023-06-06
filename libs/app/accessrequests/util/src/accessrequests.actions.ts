8d4724cbe2a071c6119c5f602e0f9cdc4c480335
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-06-06 13:05:17 +0200

    Added action for rejecting access request

--- libs/app/accessrequests/util/src/accessrequests.actions.ts
+++ libs/app/accessrequests/util/src/accessrequests.actions.ts
@@ -2,3 +2,8 @@ export class GetAccessRequests {
     static readonly type = '[AccessRequests] Get Access Requests';
     constructor(public readonly eventName: string) { }
 }
+
+export class RejectAccessRequest {
+    static readonly type = '[AccessRequests] Reject Access Request';
+    constructor(public readonly userId: string) { }
+}
