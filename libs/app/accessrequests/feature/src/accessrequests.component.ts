6e566884cdd8a8c814bdeaaddc1cb1648dc51ae6
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-06-06 13:04:02 +0200

    Added functionality to close modal controller

--- libs/app/accessrequests/feature/src/accessrequests.component.ts
+++ libs/app/accessrequests/feature/src/accessrequests.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'event-participation-trends-accessrequests',
   templateUrl: './accessrequests.component.html',
   styleUrls: ['./accessrequests.component.css'],
 })
-export class AccessRequestsComponent {}
+export class AccessRequestsComponent {
+
+  constructor(private modalController : ModalController) { }
+
+  async closeAccessRequests() {
+    await this.modalController.dismiss();
+  }
+}
