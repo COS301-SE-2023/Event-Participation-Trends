b97b30cfd45f1c54423fd7bd4b76eb12497da3eb
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-06-06 13:20:21 +0200

    Added functions to dispatch approve/reject actions

--- libs/app/accessrequests/feature/src/accessrequests.component.ts
+++ libs/app/accessrequests/feature/src/accessrequests.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AccessRequestsState } from '@event-participation-trends/app/accessrequests/data-access';
+import { Select, Store } from '@ngxs/store';
+import { ApproveAccessRequest, RejectAccessRequest } from '@event-participation-trends/app/accessrequests/util';
 
 @Component({
   selector: 'event-participation-trends-accessrequests',
@@ -7,10 +10,20 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./accessrequests.component.css'],
 })
 export class AccessRequestsComponent {
+  @Select(AccessRequestsState.accessRequests) accessRequests$!: any[];
 
-  constructor(private modalController : ModalController) { }
+  constructor(private modalController : ModalController, private store: Store) { }
 
   async closeAccessRequests() {
     await this.modalController.dismiss();
   }
+
+  approveAccessRequest(userId: string) {
+    this.store.dispatch(new ApproveAccessRequest(userId));
+  }
+
+  rejectAccessRequest(userId: string) {
+    this.store.dispatch(new RejectAccessRequest(userId));
+  }
+
 }
