d600d2a639c4e1bed86384c15e5143746b9e0fe3
LucaLoubser <u20439963@tuks.co.za>
2023-06-07 22:16:23 +0200

    further linking for endpoint

--- libs/api/core/feature/src/core.module.ts
+++ libs/api/core/feature/src/core.module.ts
@@ -1,7 +1,10 @@
-import { ViewerModule } from '@event-participation-trends/api/viewer/feature';
+import { ViewerModule, ViewerService } from '@event-participation-trends/api/viewer/feature';
+import { ViewerController } from '../src/controllers/viewer.controller'
 import { Module } from '@nestjs/common';
 
 @Module({
     imports: [ViewerModule],
+    controllers: [ViewerController],
+    exports: [ViewerService]
 })
 export class CoreModule {}
