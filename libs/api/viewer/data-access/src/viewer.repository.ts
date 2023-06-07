2d4aa65386ab681db1fc09052c40a8a9a4310cce
LucaLoubser <u20439963@tuks.co.za>
2023-06-07 15:15:41 +0200

    added viewer repository

--- libs/api/viewer/data-access/src/viewer.repository.ts
+++ libs/api/viewer/data-access/src/viewer.repository.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from '@event-participation-trends/api/viewer/util';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import {User} from '../schemas';
 
 @Injectable()
-export class ViewerRepository {}
+export class ViewerRepository {
+    constructor(
+        @InjectModel(User.name)
+        private userModel: mongoose.Model<User>
+    ){}
+
+    async createViewer(user: IUser){
+        await this.userModel.create(user);
+    }    
+}
