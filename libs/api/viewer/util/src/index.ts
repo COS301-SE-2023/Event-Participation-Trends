058a8e9e011cbb4fe4673fa60ceaeec113297576
LucaLoubser <u20439963@tuks.co.za>
2023-06-07 15:08:12 +0200

    added utils folder linking

--- libs/api/viewer/util/src/index.ts
+++ libs/api/viewer/util/src/index.ts
@@ -1,1 +1,7 @@
 export * from './lib/api-viewer-util';
+export * from './requests';
+export * from './responses';
+export * from './enums';
+export * from './commands';
+export * from './interfaces';
+export * from './events';
