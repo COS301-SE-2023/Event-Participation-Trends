ba140d8f1e35baf78af6768e52a349068726e387
LucaLoubser <u20439963@tuks.co.za>
2023-06-07 12:43:57 +0200

    modified stall.schema.ts

--- libs/api/viewer/data-access/schemas/stall.schema.ts
+++ libs/api/viewer/data-access/schemas/stall.schema.ts
@@ -4,6 +4,17 @@ import { HydratedDocument } from 'mongoose';
 export type StallDocument = HydratedDocument<Stall>;
 
 @Schema({timestamps: true, collection: 'Stall' })
-export class Stall{}
+export class Stall{
+    
+    @Prop({ required: true })
+    Name: string | undefined | null;
+
+    @Prop({ required: true })
+    x_coordinate: number | undefined | null;
+
+    @Prop({ required: true })
+    y_coordinate: number | undefined | null;
+
+}
 
 export const StallSchema = SchemaFactory.createForClass(Stall);
