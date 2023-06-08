1f8b7a32daf3a453961907058f0bcda73e054ae4
LucaLoubser <u20439963@tuks.co.za>
2023-06-08 23:46:40 +0200

    linking for user module (3)

--- libs/api/user/util/src/index.ts
+++ libs/api/user/util/src/index.ts
@@ -1,1 +1,8 @@
 export * from './lib/api-user-util';
+export* from './requests';
+export * from './responses';
+export * from './enums';
+export * from './interfaces';
+export * from './events';
+export * from './queries';
+export * from './commands';
