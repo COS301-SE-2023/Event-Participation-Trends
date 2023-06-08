5afddf3ae09b7a26f5f5219d2bde86f92e60c2bb
LucaLoubser <u20439963@tuks.co.za>
2023-06-08 23:48:17 +0200

    unlinked the viewer feature (3)

--- libs/api/viewer/util/src/index.ts
+++ libs/api/viewer/util/src/index.ts
@@ -1,7 +1,1 @@
 export * from './lib/api-viewer-util';
-export * from './requests';
-export * from './responses';
-export * from './enums';
-export * from './commands';
-export * from './interfaces';
-export * from './events';
