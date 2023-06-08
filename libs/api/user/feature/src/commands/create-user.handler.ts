6d66f4f24ae4d3ccaf2b0a4c0c934531a45e7a00
LucaLoubser <u20439963@tuks.co.za>
2023-06-08 23:58:09 +0200

    migration of viewer to user feature (2)

---
+++ libs/api/user/feature/src/commands/create-user.handler.ts
@@ -0,0 +1,39 @@
+import { UserRepository } from '@event-participation-trends/api/user/data-access';
+import { CreateUserCommand, IUser, ICreateUserResponse } from '@event-participation-trends/api/user/util';
+import { Status, Role } from'@event-participation-trends/api/user/util';
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
+import { User } from '../models';
+ 
+@CommandHandler(CreateUserCommand)
+export class CreateUserHandler implements ICommandHandler<CreateUserCommand, ICreateUserResponse> {
+    constructor(
+        private readonly publisher: EventPublisher,
+        private readonly userRepository: UserRepository,
+      ) {}
+    
+      async execute(command: CreateUserCommand) {
+        console.log(`${CreateUserCommand.name}`);
+
+        const request = command.request.user;
+
+        if (!request.Email)
+            throw new Error('Missing required field: Eamil');
+
+        const userDoc= await this.userRepository.getUser(request.Email || "");
+        if(userDoc.length == 0){
+            const data: IUser = {
+                Email: request.Email,
+                FirstName: request.FirstName,
+                LastName: request.LastName,
+                photo: request.photo,
+                Role: Role.VIEWER,
+            }
+    
+            const user = this.publisher.mergeObjectContext(User.fromData(data));
+            user.create();
+            user.commit();
+        }
+
+        return { status : Status.SUCCESS };
+      }
+}
