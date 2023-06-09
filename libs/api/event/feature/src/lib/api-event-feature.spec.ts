9b0dd3cc33c2d332b8fb42c56d7cadfd322dbd66
LucaLoubser <u20439963@tuks.co.za>
2023-06-09 14:26:00 +0200

    addes scaffolding for Event feature

---
+++ libs/api/event/feature/src/lib/api-event-feature.spec.ts
@@ -0,0 +1,7 @@
+import { apiEventFeature } from './api-event-feature';
+
+describe('apiEventFeature', () => {
+  it('should work', () => {
+    expect(apiEventFeature()).toEqual('api-event-feature');
+  });
+});
