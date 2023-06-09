566a7bfc04687272445eb3c44e720bc8796c4ef7
LucaLoubser <u20439963@tuks.co.za>
2023-06-09 13:04:01 +0200

    linking for getUsers endpoint (2)

--- libs/api/core/feature/src/core.module.ts
+++ libs/api/core/feature/src/core.module.ts
@@ -1,10 +1,12 @@
 import { UserModule, UserService } from '@event-participation-trends/api/user/feature';
 import { UserController } from '../src/controllers/user.controller'
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-    imports: [UserModule],
+    imports: [CqrsModule, UserModule],
     controllers: [UserController],
+    providers: [UserService],
     exports: [UserService]
 })
 export class CoreModule {}
