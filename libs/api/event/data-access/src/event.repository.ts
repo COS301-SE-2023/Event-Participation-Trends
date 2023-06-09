82cd4204928c1eee5cf39ff44a8e41a571969873
LucaLoubser <u20439963@tuks.co.za>
2023-06-09 14:40:24 +0200

    added event repository for DB interaction

---
+++ libs/api/event/data-access/src/event.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { IUser } from '@event-participation-trends/api/user/util';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Event,
+         Device,
+         FloorLayout,
+         DeviceLocation,
+         EventLocation,
+         Sensor,
+         Stall,
+         TEMP_DEVICE_BUFFER,
+         TEMP_DEVICE_TO_DT,
+} from '../schemas';
+
+@Injectable()
+export class EventRepository {
+    constructor(
+        @InjectModel(Event.name) private eventModel: mongoose.Model<Event>,
+        @InjectModel(Device.name) private deviceModel: mongoose.Model<Device>,
+        @InjectModel(FloorLayout.name) private floorLayoutModel: mongoose.Model<FloorLayout>,
+        @InjectModel(DeviceLocation.name) private deviceLocationModel: mongoose.Model<DeviceLocation>,
+        @InjectModel(EventLocation.name) private EventLocationModel: mongoose.Model<EventLocation>,
+        @InjectModel(Sensor.name) private sensorModel: mongoose.Model<Sensor>,
+        @InjectModel(Stall.name) private stallModel: mongoose.Model<Stall>,
+        @InjectModel(TEMP_DEVICE_BUFFER.name) private TEMP_DEVICE_BUFFERModel: mongoose.Model<TEMP_DEVICE_BUFFER>,
+        @InjectModel(TEMP_DEVICE_TO_DT.name) private TEMP_DEVICE_TO_DTModel: mongoose.Model<TEMP_DEVICE_TO_DT>,
+    ){}
+
+}
