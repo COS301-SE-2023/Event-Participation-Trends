0245853e98b4b99f658fca445c0317c224872f95
Stefan van der Merwe <u21429121@tuks.co.za>
2023-06-09 17:49:42 +0200

    Implement initial view-events

--- libs/app/viewevents/feature/src/viewevents.page.ts
+++ libs/app/viewevents/feature/src/viewevents.page.ts
@@ -1,8 +1,49 @@
 import { Component } from '@angular/core';
 
+interface Event {
+  name: string;
+  location: string;
+  category: string;
+  hasAccess: boolean;
+}
+
 @Component({
   selector: 'event-participation-trends-viewevents',
   templateUrl: './viewevents.page.html',
   styleUrls: ['./viewevents.page.css'],
 })
-export class VieweventsPage {}
+export class VieweventsPage {
+  events: Event[] = [
+    // Mock event names, categories and locations. They have to be believable.
+    {
+      name: 'Polar Bear Plunge',
+      location: 'Antarctica',
+      category: 'Swimming',
+      hasAccess: true,
+    },
+    {
+      name: 'The Great Wall Marathon',
+      location: 'China',
+      category: 'Running',
+      hasAccess: true,
+    },
+    {
+      name: 'The Color Run',
+      location: 'United States',
+      category: 'Running',
+      hasAccess: true,
+    },
+    {
+      name: 'The Great Barrier Reef Marathon Festival',
+      location: 'Australia',
+      category: 'Running',
+      hasAccess: true,
+    },
+    {
+      name: 'The Great Wall Marathon',
+      location: 'China',
+      category: 'Running',
+      hasAccess: true,
+    }    
+  ];
+}
