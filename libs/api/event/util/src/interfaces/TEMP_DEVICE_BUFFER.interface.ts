d73e58d519d59029713adab145b1d5010a3caf85
LucaLoubser <u20439963@tuks.co.za>
2023-06-10 14:09:20 +0200

    added interfaces for all schemas

---
+++ libs/api/event/util/src/interfaces/TEMP_DEVICE_BUFFER.interface.ts
@@ -0,0 +1,7 @@
+
+export interface ITEMP_DEVICE_BUFFER{
+    DeviceId: string | undefined | null,
+    SensorId: string | undefined | null,
+    TimeStamp: Date | undefined | null,
+    SignalStrenght: number | undefined | null,
+}
