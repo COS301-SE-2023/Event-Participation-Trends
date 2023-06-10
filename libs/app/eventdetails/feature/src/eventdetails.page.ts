c3ab2bedab273ac3d5f7a84635d347b2c04ded7a
Lukas Anthonissen <lukasanthonissen@gmail.com>
2023-06-10 12:26:09 +0200

    Add container in eventdetails

--- libs/app/eventdetails/feature/src/eventdetails.page.ts
+++ libs/app/eventdetails/feature/src/eventdetails.page.ts
@@ -1,5 +1,11 @@
+import { Time } from '@angular/common';
 import { Component } from '@angular/core';
 
+interface AccessRequest {
+  id: number;
+  email: string;
+  timestamp: number;
+}
 @Component({
   selector: 'event-participation-trends-eventdetails',
   templateUrl: './eventdetails.page.html',
@@ -7,4 +13,26 @@ import { Component } from '@angular/core';
 })
 export class EventDetailsPage {
   
+  public accessRequests: AccessRequest[] = [
+    {
+      id: 1,
+      email: 'user_1@gmail.com',
+      timestamp: 1620000000000,
+    },
+    {
+      id: 2,
+      email: 'user_2@gmail.com',
+      timestamp: 1620000000000,
+    },
+  ];
+
+  allowAccess(accessRequest: AccessRequest) {
+    console.log('allowAccess', accessRequest);
+  }
+
+  denyAccess(accessRequest: AccessRequest) {
+    console.log('denyAccess', accessRequest);
+  }
+
+  showRequests = true;
 }
