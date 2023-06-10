514862d7a2f13b611b38404692fd16347642ff55
LucaLoubser <u20439963@tuks.co.za>
2023-06-10 17:26:12 +0200

    added query handler

---
+++ libs/api/event/feature/src/queries/get-managed-events.handler.ts
@@ -0,0 +1,36 @@
+import { UserRepository } from '@event-participation-trends/api/user/data-access';
+import { EventRepository } from '@event-participation-trends/api/event/data-access';
+import { GetManagedEventsQuery, IGetManagedEventsResponse } from '@event-participation-trends/api/event/util';
+import { Role } from '@event-participation-trends/api/user/util';
+import { QueryHandler, IQueryHandler } from '@nestjs/cqrs';
+
+@QueryHandler(GetManagedEventsQuery)
+export class GetManagedEventsHandler implements IQueryHandler<GetManagedEventsQuery, IGetManagedEventsResponse> {
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly eventRepository: EventRepository
+    ) {}
+
+    async execute(query: GetManagedEventsQuery) {
+        console.log(`${GetManagedEventsHandler.name}`);
+        const request = query.request;
+
+        if (!request.ManagerEmail)
+            throw new Error('Missing required field: ManagerEmail');
+        
+        const managerDoc = await this.userRepository.getUser(request.ManagerEmail);
+        if(managerDoc.length == 0)
+            throw new Error(`user with email ${request.ManagerEmail} does not exist in DB`);
+        else {
+            if(managerDoc[0].Role === Role.MANAGER){
+                const eventDocs = await this.eventRepository.getManagedEvents(managerDoc[0]._id);
+                if(eventDocs.length == 0)
+                    throw new Error(`User with email ${request.ManagerEmail} does not manage any events`);
+                else
+                    return {events: eventDocs};
+            }else{
+                throw new Error(`User with email ${request.ManagerEmail} does not have manager Privileges`);
+            }
+        }
+    } 
+}
