c4b79c8014600670271363985e92cd52e6152977
LucaLoubser <u20439963@tuks.co.za>
2023-06-10 13:41:29 +0200

    added linking for createEvent endpoint

--- libs/api/core/feature/src/core.module.ts
+++ libs/api/core/feature/src/core.module.ts
@@ -2,11 +2,13 @@ import { UserModule, UserService } from '@event-participation-trends/api/user/fe
 import { UserController } from '../src/controllers/user.controller'
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { EventController } from '../src/controllers/event.controller';
+import { EventService, EventModule } from '@event-participation-trends/api/event/feature';
 
 @Module({
-    imports: [CqrsModule, UserModule],
-    controllers: [UserController],
-    providers: [UserService],
-    exports: [UserService]
+    imports: [CqrsModule, UserModule, EventModule],
+    controllers: [UserController, EventController],
+    providers: [UserService, EventService],
+    exports: [UserService ,EventService],
 })
 export class CoreModule {}
