3f79c4a1839778324063d10a47e236f4d7510b20
LucaLoubser <u20439963@tuks.co.za>
2023-06-10 13:46:13 +0200

    added linking for createEvent endpoint

--- libs/api/event/util/src/index.ts
+++ libs/api/event/util/src/index.ts
@@ -1,1 +1,6 @@
 export * from './lib/api-event-util';
+export * from './commands';
+export * from './events';
+export * from './interfaces'
+export * from './requests';
+export * from './responses';
