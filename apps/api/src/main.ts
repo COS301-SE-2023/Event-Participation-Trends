4240ce5b3252f9f3b6ec461e7777a4ab2145e8ec
Stefan van der Merwe <u21429121@tuks.co.za>
2023-06-12 15:53:30 +0200

    Remove helmet for now. Will add back later.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,28 +15,28 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.use(cookieParser());
-  app.use(helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'"],
-        styleSrc: ["'self'"],
-        imgSrc: ["'self'"],
-        connectSrc: ["'self'"],
-        fontSrc: ["'self'"],
-        objectSrc: ["'none'"],
-        mediaSrc: ["'self'"],
-        frameSrc: ["'none'"],
-        childSrc: ["'none'"],
-        formAction: ["'self'"],
-      },
-    },
-    frameguard: { action: 'deny' },
-    hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
-    ieNoOpen: true,
-    noSniff: true,
-    xssFilter: true,
-  }));
+  // app.use(helmet({
+  //   contentSecurityPolicy: {
+  //     directives: {
+  //       defaultSrc: ["'self'"],
+  //       scriptSrc: ["'self'"],
+  //       styleSrc: ["'self'"],
+  //       imgSrc: ["'self'"],
+  //       connectSrc: ["'self'"],
+  //       fontSrc: ["'self'"],
+  //       objectSrc: ["'none'"],
+  //       mediaSrc: ["'self'"],
+  //       frameSrc: ["'none'"],
+  //       childSrc: ["'none'"],
+  //       formAction: ["'self'"],
+  //     },
+  //   },
+  //   frameguard: { action: 'deny' },
+  //   hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
+  //   ieNoOpen: true,
+  //   noSniff: true,
+  //   xssFilter: true,
+  // }));
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
