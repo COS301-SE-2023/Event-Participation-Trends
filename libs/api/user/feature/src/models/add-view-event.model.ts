7462657e271ff2a1f3ceba4dbb024ea82b4d0351
LucaLoubser <u20439963@tuks.co.za>
2023-06-12 19:32:48 +0200

    added model for adding an event to a users Viewing list

---
+++ libs/api/user/feature/src/models/add-view-event.model.ts
@@ -0,0 +1,35 @@
+import { 
+    IAddViewEvent, 
+    AddViewingEventEvent, 
+    } from '@event-participation-trends/api/user/util';
+import { AggregateRoot } from '@nestjs/cqrs';
+
+export class AddViewEvent extends AggregateRoot implements IAddViewEvent {
+    constructor(
+        public userEmail: string | undefined | null,
+        public eventId: string | undefined | null,
+    ){
+        super();
+    }
+
+    add() {
+        this.apply(new AddViewingEventEvent(this.toJSON()));
+    }
+
+    static fromData(event: IAddViewEvent): AddViewEvent {
+        const instance = new AddViewEvent(
+          event.userEmail,
+          event.eventId,
+        );
+    
+        return instance;
+    }
+
+    toJSON(): IAddViewEvent {
+        return {
+            userEmail: this.userEmail,
+            eventId: this.eventId,
+        };
+    }
+
+}
