8a827899b9a0456c0ed4d346b5f5ec1268a88bb4
LucaLoubser <u20439963@tuks.co.za>
2023-06-12 14:48:49 +0200

    modified linking

--- libs/api/event/feature/src/models/index.ts
+++ libs/api/event/feature/src/models/index.ts
@@ -1,4 +1,4 @@
 export * from './event.model';
 export * from './event-details.model';
 export * from './view-event.model';
-export * from './decline-view.model';
+export * from './view-request.model';
