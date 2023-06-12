8d7ee023e3675fb7f68e9113c289e7aed5812847
LucaLoubser <u20439963@tuks.co.za>
2023-06-12 15:28:24 +0200

    updated user interface

--- libs/api/user/util/src/interfaces/user.interface.ts
+++ libs/api/user/util/src/interfaces/user.interface.ts
@@ -1,8 +1,9 @@
-
+import { Types } from 'mongoose';
 export interface IUser {
 	Email?: string | undefined | null;
 	FirstName?: string | undefined | null;
 	LastName?: string | undefined | null;
 	photo?: string | undefined | null ;
 	Role?: string | undefined | null;
+    Viewing?: Types.ObjectId[] | undefined | null;
 }
