bf81c7ab86a75ed6c62b7b2059654c3b442ff8d8
LucaLoubser <u20439963@tuks.co.za>
2023-06-12 14:48:40 +0200

    modified linking

--- libs/api/event/feature/src/events/index.ts
+++ libs/api/event/feature/src/events/index.ts
@@ -1,3 +1,3 @@
 export * from './create-event.handler';
 export * from './send-view-request-event.handler';
-export * from './deceline-view-request.event.handler';
+export * from './decline-view-request.event.handler';
