8f972b2048734d1f88f973796b23147fdf79ca53
Arno <u21457451@tuks.co.za>
2023-06-14 22:53:13 +0200

    Added validation to time inputs

--- libs/app/addevent/feature/src/addevent.page.ts
+++ libs/app/addevent/feature/src/addevent.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'event-participation-trends-addevent',
@@ -54,5 +55,50 @@ export class AddEventPage implements OnInit {
   get eventEndTime() {
     return this.eventForm.get('eventEndTime');
   }
+
+  // onStartTimeChange() {
+  //   const startTime = this.eventForm.get('eventStartTime')?.value;
+  //   console.log("Start Time: " + startTime);
+  //   let endTime = this.eventForm.get('eventEndTime')?.value;
+
+  //   if (!endTime) {
+  //     endTime = this.eventForm.get('eventEndTime')?.setValue(startTime);
+  //   }
+  
+  //   if (((startTime && !endTime) || (startTime && endTime)) && startTime >= endTime) {
+  //     // const newEndTime = new Date(startTime);
+  //     // newEndTime.setHours(newEndTime.getHours() + 1);
+  //     this.eventForm.patchValue({ eventEndTime: endTime.toISOString().slice(0, -8) });
+  //     // this.eventForm.get('eventEndTime')?.setValue(newEndTime.toISOString().slice(0, -8));
+  //     console.log("End Time: " + endTime);
+  //   }
+  // }
+
+  onStartTimeChange() {
+    const startTime = this.eventForm.get('eventStartTime')?.value;
+    let endTime = this.eventForm.get('eventEndTime')?.value;
+
+    if (!endTime) {
+      endTime = this.eventForm.get('eventEndTime')?.setValue(startTime);
+    }
+    else if (startTime && endTime && startTime >= endTime) {
+      const newEndTime = moment(startTime, 'HH:mm').add(1, 'hours').format('HH:mm');
+      this.eventForm.patchValue({ eventEndTime: newEndTime });
+    }
+  }
+
+  onEndTimeChange() {
+    let startTime = this.eventForm.get('eventStartTime')?.value;
+    const endTime = this.eventForm.get('eventEndTime')?.value;
+
+    if (!startTime) {
+      startTime = this.eventForm.get('eventStartTime')?.setValue(endTime);
+    }
+    else if (endTime && startTime && endTime <= startTime) {
+      const newStartTime = moment(endTime, 'HH:mm').subtract(1, 'hours').format('HH:mm');
+      this.eventForm.patchValue({ eventStartTime: newStartTime });
+    }
+  }
+  
 
 }
