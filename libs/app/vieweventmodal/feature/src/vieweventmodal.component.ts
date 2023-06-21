c12248e4787f5cd9a8a5fae2bfec70f47a86cca5
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-06-21 20:27:46 +0200

    Fixed positioning of buttons and images

--- libs/app/vieweventmodal/feature/src/vieweventmodal.component.ts
+++ libs/app/vieweventmodal/feature/src/vieweventmodal.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ModalController, AlertController, ToastController } from '@ionic/angular';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'event-participation-trends-vieweventmodal',
   templateUrl: './vieweventmodal.component.html',
   styleUrls: ['./vieweventmodal.component.css'],
 })
-export class ViewEventModalComponent {}
+export class ViewEventModalComponent {
+  @Input() eventName: string | undefined;
+  @Input() eventId: string | undefined;
+
+  handlerMessage = '';
+
+  constructor(private modalController : ModalController, private store: Store, private readonly alertController: AlertController, private readonly toastController: ToastController) { }
+
+  async closeModal() {
+    await this.modalController.dismiss();
+  }
+}
