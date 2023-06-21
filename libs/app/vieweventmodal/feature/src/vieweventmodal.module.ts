de056cf18dfd8fd49b4d668d04f95730f054db6b
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-06-21 21:59:03 +0200

    Added routing from modal

--- libs/app/vieweventmodal/feature/src/vieweventmodal.module.ts
+++ libs/app/vieweventmodal/feature/src/vieweventmodal.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ViewEventModalComponent } from './vieweventmodal.component';
+import { VieweventmodalRoutingModule } from './vieweventmodal.routing';
 
 @NgModule({
   declarations: [ViewEventModalComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    VieweventmodalRoutingModule
   ]
 })
 export class VieweventmodalModule { }
