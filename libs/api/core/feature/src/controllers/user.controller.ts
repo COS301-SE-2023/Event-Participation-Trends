0d22f65aa474b87d1c4bdf5cc867c34b0535cad2
Stefan van der Merwe <u21429121@tuks.co.za>
2023-06-23 15:40:41 +0200

    Update getRole endpoint

--- libs/api/core/feature/src/controllers/user.controller.ts
+++ libs/api/core/feature/src/controllers/user.controller.ts
@@ -1,58 +1,70 @@
 import { UserService } from '@event-participation-trends/api/user/feature';
 import {
-    IGetUsersRequest,
-    IGetUsersResponse,
-    IUpdateRoleRequest,
-    IupdateRoleResponse,
-    Role,
+    IGetUserRoleResponse,
+  IGetUsersRequest,
+  IGetUsersResponse,
+  IUpdateRoleRequest,
+  IupdateRoleResponse,
+  Role,
 } from '@event-participation-trends/api/user/util';
-import { Body, Controller, Post, Get, UseGuards, Req, SetMetadata } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  UseGuards,
+  Req,
+  SetMetadata,
+} from '@nestjs/common';
 import { Request } from 'express';
-import { JwtGuard, RbacGuard, CsrfGuard } from '@event-participation-trends/api/guards';
+import {
+  JwtGuard,
+  RbacGuard,
+  CsrfGuard,
+} from '@event-participation-trends/api/guards';
 
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService){}
+  constructor(private userService: UserService) {}
 
-    @Get('getAllUsers')
-    @SetMetadata('role',Role.ADMIN)
-    @UseGuards(JwtGuard, RbacGuard, CsrfGuard)
-    async getAllUsers(
-        @Req() req: Request,
-    ): Promise<IGetUsersResponse> {
-        const request: any =req;
-        const extractRequest: IGetUsersRequest = {
-            AdminEmail: request.user["email"]
-        }
-        return this.userService.getUsers(extractRequest);
-    }
+  @Get('getAllUsers')
+  @SetMetadata('role', Role.ADMIN)
+  @UseGuards(JwtGuard, RbacGuard, CsrfGuard)
+  async getAllUsers(@Req() req: Request): Promise<IGetUsersResponse> {
+    const request: any = req;
+    const extractRequest: IGetUsersRequest = {
+      AdminEmail: request.user['email'],
+    };
+    return this.userService.getUsers(extractRequest);
+  }
 
-    @Post('updateUserRole')
-    @SetMetadata('role',Role.ADMIN)
-    @UseGuards(JwtGuard, RbacGuard, CsrfGuard)
-    async updateUserRole(
-        @Req() req: Request,
-        @Body() requestBody: IUpdateRoleRequest,
-    ): Promise<IupdateRoleResponse> {
-        const request: any =req;
-        console.log(request.user["email"])
-        const extractRequest: IUpdateRoleRequest = {
-            update: {
-                AdminEmail: request.user["email"],
-                UserEmail: requestBody.update.UserEmail,
-                UpdateRole: requestBody.update.UpdateRole
-            }
-        }
-        return this.userService.updateUserRole(extractRequest);
-    }
+  @Post('updateUserRole')
+  @SetMetadata('role', Role.ADMIN)
+  @UseGuards(JwtGuard, RbacGuard, CsrfGuard)
+  async updateUserRole(
+    @Req() req: Request,
+    @Body() requestBody: IUpdateRoleRequest
+  ): Promise<IupdateRoleResponse> {
+    const request: any = req;
+    console.log(request.user['email']);
+    const extractRequest: IUpdateRoleRequest = {
+      update: {
+        AdminEmail: request.user['email'],
+        UserEmail: requestBody.update.UserEmail,
+        UpdateRole: requestBody.update.UpdateRole,
+      },
+    };
+    return this.userService.updateUserRole(extractRequest);
+  }
 
-    @Get('getRole')
-    @SetMetadata('role',Role.VIEWER)
-    @UseGuards(JwtGuard, RbacGuard, CsrfGuard)
-    async getRole(
-        @Req() req: Request,
-    ): Promise<Role> {
-        const request: any =req;
-        return request.user["role"];
-    }
+  @Get('getRole')
+  @SetMetadata('role', Role.VIEWER)
+  @UseGuards(JwtGuard, RbacGuard, CsrfGuard)
+  async getRole(@Req() req: Request): Promise<IGetUserRoleResponse> {
+    const request: any = req;
+
+    return {
+        userRole: request.user['role'],
+    };
+  }
 }
