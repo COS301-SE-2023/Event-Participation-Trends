8aa339cd8c42dc881cb32103620935c43ca54a80
LucaLoubser <u20439963@tuks.co.za>
2023-06-24 00:40:46 +0200

    added linking for events

--- libs/api/event/util/src/events/index.ts
+++ libs/api/event/util/src/events/index.ts
@@ -2,3 +2,5 @@ export * from './create-event.event';
 export * from './send-view-request.event';
 export * from './decline-view-request.event';
 export * from './accept-view-request.event';
+export * from './remove-viewer-from-event.event';
+export * from './remove-event-from-viewer.event';
