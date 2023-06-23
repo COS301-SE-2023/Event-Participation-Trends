73b47a25c52d5be3d1485c42dfd881519049a2c6
LucaLoubser <u20439963@tuks.co.za>
2023-06-23 23:06:48 +0200

    added linking for request

--- libs/api/event/util/src/requests/index.ts
+++ libs/api/event/util/src/requests/index.ts
@@ -5,3 +5,4 @@ export * from './send-view-request.request';
 export * from './get-all-view-requests.request';
 export * from './decline-view-request.request';
 export * from './accept-veiw-request.request';
+export * from './get-all-viewer-events.request';
