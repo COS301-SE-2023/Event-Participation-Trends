09f13d6ed36723659c6a884320db592271da6633
LucaLoubser <u20439963@tuks.co.za>
2023-06-24 13:38:49 +0200

    added linking for model

--- libs/api/event/feature/src/models/index.ts
+++ libs/api/event/feature/src/models/index.ts
@@ -3,3 +3,4 @@ export * from './event-details.model';
 export * from './view-event.model';
 export * from './view-request.model';
 export * from './remove-viewer.model';
+export * from './update-event-details.model';
