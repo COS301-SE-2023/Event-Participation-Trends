4aedaf33a4bea8a02910fea996afaa0f309efefc
LucaLoubser <u20439963@tuks.co.za>
2023-06-28 22:56:31 +0200

    added linking for response

--- libs/api/user/util/src/responses/index.ts
+++ libs/api/user/util/src/responses/index.ts
@@ -3,3 +3,4 @@ export * from './create-user.response';
 export * from './update-role.response';
 export * from './get-user-role.response';
 export * from './add-viewing-event.response';
+export * from './add-viewing-event-by-name.response';
