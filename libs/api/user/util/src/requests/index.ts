cd443d3b46615bc19821e32dcb5651d830347670
LucaLoubser <u20439963@tuks.co.za>
2023-06-28 23:52:34 +0200

    added linking for request

--- libs/api/user/util/src/requests/index.ts
+++ libs/api/user/util/src/requests/index.ts
@@ -4,3 +4,4 @@ export * from './update-role.request';
 export * from './get-user-role.request';
 export * from './add-viewing-event.request';
 export * from './add-viewint-event-by-name.request';
+export * from './add-event-to-admin.request';
