a848c946b97a0e9b5d05eba20cdbdf195c77a655
LucaLoubser <u20439963@tuks.co.za>
2023-06-28 23:51:53 +0200

    added linking for command

--- libs/api/user/util/src/commands/index.ts
+++ libs/api/user/util/src/commands/index.ts
@@ -2,3 +2,4 @@ export * from './create-user-command';
 export * from './update-user-role.command';
 export * from './add-viewing-event.command';
 export * from './add-viewing-event-by-name.command';
+export * from './add-event-to-admin.command';
