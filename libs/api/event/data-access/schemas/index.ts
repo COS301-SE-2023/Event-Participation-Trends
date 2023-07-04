17a749067d1c722b7fb57606533474b614c5b9ac
LucaLoubser <u20439963@tuks.co.za>
2023-07-04 15:43:24 +0200

    updated linking

--- libs/api/event/data-access/schemas/index.ts
+++ libs/api/event/data-access/schemas/index.ts
@@ -3,5 +3,4 @@ export * from './device.schema';
 export * from './floorlayout.schema';
 export * from './sensor.schema';
 export * from './stall.schema';
-export * from './TEMP_DEVICE_TO_DT.schema';
 export * from './Eventlocation.schema';
--- libs/api/event/data-access/src/index.ts
+++ libs/api/event/data-access/src/index.ts
@@ -2,4 +2,3 @@ export * from './lib/api-event-data-access';
 export * from './event.module';
 export * from './event.repository';
 export * from '../schemas';
-export * from '../src/interfaces'
