9e077c13ffc13a028da8412a8dc21e77097ab8c1
LucaLoubser <u20439963@tuks.co.za>
2023-07-04 15:43:00 +0200

    updated event interface, removed attributes for storing data received from sensors and anonomizing devices

--- libs/api/event/util/src/interfaces/event.interface.ts
+++ libs/api/event/util/src/interfaces/event.interface.ts
@@ -4,8 +4,6 @@ import { IFloorLayout } from "./floorlayout.interface";
 import { IStall } from "./stall.interface";
 import { ISensor } from "./sensor.interface";
 import { Types } from 'mongoose';
-import { MqttData } from "./mqtt-data.interface";
-import { IMacToId } from "./mac-to-id.interface";
 
 export interface IEvent {
     StartDate?: Date | undefined | null;
@@ -17,8 +15,6 @@ export interface IEvent {
     Stalls?: IStall[] | undefined | null;
     Sensors?: ISensor[] | undefined | null;
     Devices?: IDevice[] | undefined | null;
-    BTIDtoDeviceBuffer?: IMacToId[] | undefined | null;
-    TEMPBuffer?: MqttData[] | undefined | null;
     Manager?: Types.ObjectId | undefined | null;
     Requesters?: Types.ObjectId[] | undefined | null;
     Viewers?: Types.ObjectId[] | undefined | null;
