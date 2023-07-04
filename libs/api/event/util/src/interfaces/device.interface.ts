519e8a768f159df60f24d9eaa8e677a1b1f219ea
LucaLoubser <u20439963@tuks.co.za>
2023-07-05 00:52:22 +0200

    removed device interface

--- libs/api/event/util/src/interfaces/device.interface.ts
+++
@@ -1,6 +0,0 @@
-import { IDeviceLocation } from './device-location.interface';
-
-export interface IDevice{
-    DeviceId: string | undefined | null,
-    Locations: IDeviceLocation[] | undefined | null
-}
