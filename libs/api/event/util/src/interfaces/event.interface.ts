e80a94a1908b4eb165a3691af4f12e2de9160629
LucaLoubser <u20439963@tuks.co.za>
2023-07-04 15:20:13 +0200

    updated event interface

--- libs/api/event/util/src/interfaces/event.interface.ts
+++ libs/api/event/util/src/interfaces/event.interface.ts
@@ -3,11 +3,7 @@ import { IEventLocation } from "./event-location.interface";
 import { IFloorLayout } from "./floorlayout.interface";
 import { IStall } from "./stall.interface";
 import { ISensor } from "./sensor.interface";
-import { ITEMP_DEVICE_TO_DT } from "./TEMP_DEVICE_TO_DT.interface";
-import { ITEMP_DEVICE_BUFFER } from "./TEMP_DEVICE_BUFFER.interface";
-import { IUser } from '@event-participation-trends/api/user/util'
 import { Types } from 'mongoose';
-import { IWall } from "./wall.interface";
 import { MqttData } from "./mqtt-data.interface";
 import { IMacToId } from "./mac-to-id.interface";
 
@@ -18,7 +14,6 @@ export interface IEvent {
     Category?: string | undefined | null;
     Location?: IEventLocation | undefined | null;
     //thisFloorLayout?: IFloorLayout | undefined | null;
-    Walls?: IWall[] | undefined | null;
     Stalls?: IStall[] | undefined | null;
     Sensors?: ISensor[] | undefined | null;
     Devices?: IDevice[] | undefined | null;
