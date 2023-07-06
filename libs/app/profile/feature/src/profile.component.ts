6e3e22421d5f00bfb4f5d0da68e0ea0d4b6635bc
Arno <u21457451@tuks.co.za>
2023-07-06 14:19:24 +0200

    Added profile component

---
+++ libs/app/profile/feature/src/profile.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-participation-trends-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+})
+export class ProfileComponent {}
