1e97451e8c31366328c36b591a9235a4145186af
LucaLoubser <u20439963@tuks.co.za>
2023-07-07 00:21:18 +0200

    setup linking for global controller

--- libs/api/core/feature/src/core.module.ts
+++ libs/api/core/feature/src/core.module.ts
@@ -1,22 +1,27 @@
-import {
-  UserModule,
-  UserService,
-} from '@event-participation-trends/api/user/feature';
-import { UserController } from '../src/controllers/user.controller';
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { UserController } from '../src/controllers/user.controller';
 import { EventController } from '../src/controllers/event.controller';
+import { GlobalController } from '../src/controllers/global.controller';
 import {
   EventService,
   EventModule,
 } from '@event-participation-trends/api/event/feature';
+import {
+    UserModule,
+    UserService,
+  } from '@event-participation-trends/api/user/feature';
+import {
+    GlobalModule,
+    GlobalService,
+  } from '@event-participation-trends/api/global/feature';
 import { JwtModule } from '@nestjs/jwt';
 import { ApiGuardsModule } from '@event-participation-trends/api/guards';
 
 @Module({
-    imports: [CqrsModule, UserModule, EventModule, JwtModule, ApiGuardsModule],
-    controllers: [UserController, EventController],
-    providers: [UserService, EventService],
-    exports: [UserService ,EventService],
+    imports: [CqrsModule, UserModule, EventModule, GlobalModule, JwtModule, ApiGuardsModule],
+    controllers: [UserController, EventController, GlobalController],
+    providers: [UserService, EventService, GlobalService],
+    exports: [UserService ,EventService, GlobalService],
 })
 export class CoreModule {}
