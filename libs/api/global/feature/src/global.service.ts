bd5d0ccd3ac79b4d377c3d3f95b8e7759145b0f6
LucaLoubser <u20439963@tuks.co.za>
2023-07-07 01:25:48 +0200

    added service to get global sensorIdToMacs array

--- libs/api/global/feature/src/global.service.ts
+++ libs/api/global/feature/src/global.service.ts
@@ -2,6 +2,9 @@ import {
     ICreateGlobalRequest,
     ICreateGlobalResponse,
     CreateGlobalCommand,
+    IGetGlobalRequest,
+    IGetGlobalResponse,
+    GetGlobalQuery
 } from '@event-participation-trends/api/global/util';
 import { Injectable } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
@@ -15,6 +18,10 @@ export class GlobalService {
 
     async createGlobal(request: ICreateGlobalRequest) {
         return await this.commandBus.execute<CreateGlobalCommand, ICreateGlobalResponse>(new CreateGlobalCommand(request));
+    }
+
+    async getGlobal(request: IGetGlobalRequest) {
+        return await this.queryBus.execute<GetGlobalQuery, IGetGlobalResponse>(new GetGlobalQuery(request));
     }
 
 }
