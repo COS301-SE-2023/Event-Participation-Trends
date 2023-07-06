2f8d608a1df2f4514b0967c30890c10e895dc596
LucaLoubser <u20439963@tuks.co.za>
2023-07-06 23:05:57 +0200

    added saffolding for global feature

---
+++ libs/api/global/util/src/lib/api-global-util.spec.ts
@@ -0,0 +1,7 @@
+import { apiGlobalUtil } from './api-global-util';
+
+describe('apiGlobalUtil', () => {
+  it('should work', () => {
+    expect(apiGlobalUtil()).toEqual('api-global-util');
+  });
+});
