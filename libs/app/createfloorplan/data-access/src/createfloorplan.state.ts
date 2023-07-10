029899c275c7bb7132c2d06e1ceac7908905c849
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-07-10 17:25:41 +0200

    Added interface for state model

--- libs/app/createfloorplan/data-access/src/createfloorplan.state.ts
+++ libs/app/createfloorplan/data-access/src/createfloorplan.state.ts
@@ -3,7 +3,25 @@ import { Action, State } from '@ngxs/store';
 
 // Once we know the interface for the create floor plan we can remove the comment from the line below
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface CreateFloorPlanStateModel {}
+export interface CreateFloorPlanStateModel {
+    sensors: {
+        attrs: {
+            x: number,
+            y: number,
+            width: number,
+            height: number,
+            cursor: string,
+            draggable: boolean,
+            cornerRadius: number,
+            padding: number,
+            fill: string,
+            opacity: number,
+            name: string,
+            customId: string
+        },
+        isLinked: boolean,
+    }[]
+}
 
 @State<CreateFloorPlanStateModel>({
     name: 'createfloorplan'
