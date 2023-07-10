35b20b9a699b2c38f2276dcecc5b0598aa6f8949
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-07-10 17:33:59 +0200

    Implemented state actions

--- libs/app/createfloorplan/data-access/src/createfloorplan.state.ts
+++ libs/app/createfloorplan/data-access/src/createfloorplan.state.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Action, State } from '@ngxs/store';
+import { AppApiService } from '@event-participation-trends/app/api';
+import { Action, Selector, State, Store } from '@ngxs/store';
+import { SetCreateFloorPlanState, SetSensors } from '@event-participation-trends/app/createfloorplan/util';
+import { SetError } from '@event-participation-trends/app/error/util';
 
 // Once we know the interface for the create floor plan we can remove the comment from the line below
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -28,4 +31,34 @@ export interface CreateFloorPlanStateModel {
 })
 
 @Injectable()
-export class CreateFloorPlanState {}
+export class CreateFloorPlanState {
+
+    @Selector()
+    static getSensors(state: CreateFloorPlanStateModel) {
+        return state.sensors;
+    }
+
+    constructor(
+        private store: Store,
+        private appApiService: AppApiService
+    ) {}
+
+    @Action(SetCreateFloorPlanState)
+    setCreateFloorPlanState(ctx: any, { payload }: SetCreateFloorPlanState) {
+        ctx.patchState(payload);
+    }
+
+    @Action(SetSensors)
+    setSensors(ctx: any, { payload }: SetSensors) {
+        try {
+            const state = ctx.getState();
+            const newState = {
+                ...state,
+                sensors: payload
+            };
+            return ctx.dispatch(new SetCreateFloorPlanState(newState));
+        } catch (error) {
+            return ctx.dispatch(new SetError((error as Error).message));
+        }
+    }
+}
