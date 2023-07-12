f30debe98c79f788e10c9c81cdbe8b001fd66ef5
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-07-12 11:00:50 +0200

    Added interface for viewevents state

--- libs/app/viewevents/data-access/src/viewevents.state.ts
+++ libs/app/viewevents/data-access/src/viewevents.state.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@angular/core';
+import { IEvent } from '@event-participation-trends/api/event/util';
 import { Action, State } from '@ngxs/store';
 
 // Once we know the interface for the Viewevents page we can remove the comment from the line below
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface VieweventsStateModel {}
+export interface VieweventsStateModel {
+    all_events: IEvent[];
+    subscribed_events: IEvent[];
+    unsubscribed_events: IEvent[];
+    my_events: IEvent[];
+    role: string;
+    searchValue: string;
+    address_location: string;
+}
 
 @State<VieweventsStateModel>({
-    name: 'viewevents'
+    name: 'viewevents',
+    defaults: {
+        all_events: [],
+        subscribed_events: [],
+        unsubscribed_events: [],
+        my_events: [],
+        role: 'Viewer',
+        searchValue: '',
+        address_location: ''
+    }
 })
 
 @Injectable()
