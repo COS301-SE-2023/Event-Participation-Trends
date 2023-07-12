f72ce4d51fb1dbd6d7590c55879460982e8691b8
Arno <u21457451@tuks.co.za>
2023-07-12 13:37:30 +0200

    Finished with a working zoom and dragging

--- libs/app/createfloorplan/feature/src/createfloorplan.page.ts
+++ libs/app/createfloorplan/feature/src/createfloorplan.page.ts
@@ -38,6 +38,21 @@ export class CreateFloorPlanPage {
     paths: Konva.Path[] = [];
     activePath: Konva.Path | null = null;
     onDustbin = false;
+    ctrlDown = false;
+    mouseDown = false;
+    gridBoundaries = {
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      bottom: 0,
+      right: 0,
+    };
+    stageState = {
+      stageScale: 1,
+      stageX: 0,
+      stageY: 0,
+    }
 
     constructor(
       private readonly appApiService: AppApiService,
@@ -230,8 +245,8 @@ export class CreateFloorPlanPage {
 
             this.canvasContainer = new Konva.Stage({
                 container: '#canvasElement',
-                width: width*0.9783,
-                height: height*0.925               
+                width: width*0.97,
+                height: height*0.925,             
             });
 
             this.canvas = new Konva.Layer();
@@ -240,7 +255,7 @@ export class CreateFloorPlanPage {
             this.canvasContainer.draw();
 
             //set object moving
-            // this.canvas.on('dragmove', this.onObjectMoving.bind(this));
+            this.canvas.on('dragmove', this.handleDragMove.bind(this));
 
             // Attach the mouse down event listener to start dragging lines
             this.canvasContainer.on('mousedown', this.onMouseDown.bind(this));
@@ -273,10 +288,91 @@ export class CreateFloorPlanPage {
             });
 
             window.addEventListener('keydown', (event: KeyboardEvent) => this.handleKeyDown(event));
+            window.addEventListener('keyup', (event: KeyboardEvent) => this.handleKeyUp(event));
+
+            const scaleBy = 1.1;
+
+            this.canvasContainer.on('wheel', (e) => {
+              e.evt.preventDefault();
+              this.handleScaleAndDrag(e, scaleBy);              
+            });
+
+            this.canvasContainer.scaleX(scaleBy);
+            this.canvasContainer.scaleY(scaleBy);
+            const wheelEvent = new WheelEvent('wheel', { deltaY: -1 });
+            this.canvasContainer.dispatchEvent(wheelEvent);
+            this.handleScaleAndDrag(wheelEvent, 1.1);
         }, 6);
     }
 
+    handleScaleAndDrag(e: any, scaleBy:number) {
+      const stage = e.target;
+      if (!stage) return;
+      const oldScale = stage.scaleX();
+
+      let pointer = null;
+      if (stage instanceof Konva.Stage) {
+        pointer = stage.getPointerPosition();
+      }
+      else {
+        pointer = stage.getStage().getPointerPosition();
+      }
+
+      if (!pointer) {
+        return;
+      }
+
+      const mousePointTo = {
+        x: pointer.x / oldScale - stage.x() / oldScale,
+        y: pointer.y / oldScale - stage.y() / oldScale
+      };
+
+      let direction = e.evt.deltaY < 0 ? 1 : -1;
+
+      if (e.evt.ctrlKey) {
+        direction = -direction;
+      }
+
+      const newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+      this.gridSize = newScale*10;
+
+      if (newScale <= 1 || newScale >= 5) return;
+
+      const x =
+        -(mousePointTo.x - pointer.x / newScale) * newScale;
+      const y =
+        -(mousePointTo.y - pointer.y / newScale) * newScale;
+
+      const pos = this.boundFunc({ x, y }, newScale);
+
+      this.canvasContainer.scale({ x: newScale, y: newScale });
+      this.canvasContainer.position(pos);
+    }
+
+    boundFunc(pos: any, scale: any) {
+      const stageWidth = this.canvasContainer.width();
+      const stageHeight = this.canvasContainer.height();
+  
+      const x = Math.min(0, Math.max(pos.x, stageWidth * (1 - scale)));
+      const y = Math.min(0, Math.max(pos.y, stageHeight * (1 - scale)));
+  
+      return {
+        x,
+        y
+      };
+    }
+
+    handleDragMove(e: any) {
+      if (this.ctrlDown) {
+        this.canvasContainer.position({
+          x: e.target.x(),
+          y: e.target.y()
+        });
+      }
+    }
+
     handleKeyDown(event: KeyboardEvent): void {
+      this.ctrlDown = false;
       event.preventDefault();
 
       if (this.activeItem) {
@@ -291,6 +387,26 @@ export class CreateFloorPlanPage {
           this.canvas.batchDraw();
         }
       }
+      else if (event.ctrlKey) {
+        this.ctrlDown = true;
+        document.body.style.cursor = 'grab';
+        if (this.mouseDown) {
+          document.body.style.cursor = 'grabbing';
+        }
+
+        this.canvasContainer.draggable(true);   
+        
+        this.canvasContainer.dragBoundFunc((pos) => {          
+          return this.boundFunc(pos, this.canvasContainer.scaleX());
+        });
+      }
+    }
+
+    handleKeyUp(event: KeyboardEvent): void {
+      this.ctrlDown = false;
+      this.canvasContainer.draggable(false);
+      document.body.style.cursor = 'default';
+      event.preventDefault();
     }
 
     setTransformer(mouseEvent?: Konva.Image | Konva.Group | Konva.Text, line?: Konva.Line | Konva.Path): void {
@@ -308,29 +424,16 @@ export class CreateFloorPlanPage {
 
       const node = target as Konva.Node;
       this.transformer.nodes([node]);
-      // if (target && target instanceof Konva.Line || target instanceof Konva.Path) {
-      //   if (line) {
-      //     // this.transformer.nodes([line]);
-      //     return;
-      //   }
-      // } else if (target && target instanceof Konva.Image) {
-      //   // Clicked on an existing textbox, do nothing  
-      //   this.transformer.nodes([this.activeItem]);
-      //   return;
-      // }
-      // else if (target && target instanceof Konva.Group) {
-      //   this.transformer.nodes([target]);
-      //   return;
-      // }
     }
 
     createSelectionBox(): void {
+      if (this.ctrlDown) {
+        return;
+      }
 
       const tr = new Konva.Transformer();
       this.transformers.push(tr);
       this.canvas.add(tr);
-
-      // tr.nodes([rect1, rect2]);
 
       const selectionBox = new Konva.Rect({
         fill: 'rgba(0,0,255,0.2)',
@@ -345,6 +448,10 @@ export class CreateFloorPlanPage {
       let y2: number;
 
       this.canvasContainer.on('mousedown', (e) => {
+        if (this.ctrlDown) {
+          return;
+        }
+        
         if (!this.preventCreatingWalls) {
           this.activeItem = null;
           return;
@@ -678,6 +785,11 @@ export class CreateFloorPlanPage {
       }
       
       onMouseDown(event: Konva.KonvaEventObject<MouseEvent>): void {
+        this.mouseDown = true;
+        if (this.ctrlDown) {
+          return;
+        }
+
         const target = event.target;
         if (target && target instanceof Konva.Line
           || target instanceof Konva.Path
@@ -727,6 +839,10 @@ export class CreateFloorPlanPage {
       }
       
       onMouseMove(): void {
+        if (this.ctrlDown) {
+          return;
+        }
+
         const pointer = this.canvasContainer.getPointerPosition();
         if (this.activePath) {
             const grid = this.gridSize;
@@ -749,6 +865,7 @@ export class CreateFloorPlanPage {
       
       onMouseUp(): void {
         this.openDustbin = false;
+        this.mouseDown = false;
 
         const pointer = this.canvasContainer.getPointerPosition();
         if (this.activePath) {
@@ -819,12 +936,18 @@ export class CreateFloorPlanPage {
         const width = stage.width();
         const height = stage.height();
         const gridGroup = new Konva.Group({
+          x: stage.x(),
+          y: stage.y(),
+          width: width,
+          height: height,
+          bottom: stage.y() + height,
+          right: stage.x() + width,
           draggable: false,
         });
         for (let i = 0; i < width / grid; i++) {
           const distance = i * grid;
           const horizontalLine = new Konva.Line({
-            points: [distance, 0, distance, height],
+            points: [distance, 0, distance, width],
             stroke: '#ccc',
             strokeWidth: 1,
             draggable: false,
@@ -840,6 +963,16 @@ export class CreateFloorPlanPage {
           gridGroup.add(horizontalLine);
           gridGroup.add(verticalLine);
         }
+        // get grid boundaries
+        this.gridBoundaries = {
+          x: gridGroup.x(),
+          y: gridGroup.y(),
+          width: gridGroup.width(),
+          height: gridGroup.height(),
+          bottom: gridGroup.y() + gridGroup.height(),
+          right: gridGroup.x() + gridGroup.width(),
+        };
+
         this.canvas.add(gridGroup);
         gridGroup.moveToBottom();
         this.canvas.batchDraw();
@@ -861,18 +994,19 @@ export class CreateFloorPlanPage {
     }
 
     removeGridLines(): void {
+      const elementsToRemove: any[] = [];
+
       this.canvas?.children?.forEach((child: any) => {
-        if (child.attrs.customClass === 'grid-line') {
-          child.remove();
-        }
+        child.children?.forEach((grandChild: any) => {
+          if (grandChild.attrs.customClass === 'grid-line') {
+            elementsToRemove.push(grandChild);
+          }
+        });
       });
 
-      
-      const width = this.canvasParent.nativeElement.offsetWidth;
-      const height = this.canvasParent.nativeElement.offsetHeight;
-
-      this.canvasContainer.setAttr('width', width*0.9783);
-      this.canvasContainer.setAttr('height', height*0.965);
+      elementsToRemove.forEach((element: any) => {
+        element.remove();
+      });
     }
     
       ngOnInit() {
