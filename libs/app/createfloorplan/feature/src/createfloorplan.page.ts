79d1b7a727d3a0934d0e1ab5aad35ce046911e4f
DieSeeKat <lukasanthonissen@gmail.com>
2023-07-12 18:26:06 +0200

    Fix floorplan zoom offset

--- libs/app/createfloorplan/feature/src/createfloorplan.page.ts
+++ libs/app/createfloorplan/feature/src/createfloorplan.page.ts
@@ -140,7 +140,7 @@ export class CreateFloorPlanPage implements OnInit{
             const positionY = this.canvasContainer.getPointerPosition()?.y || 0;
             const droppedItem: DroppedItem = { name };
             this.canvasItems.push(droppedItem);
-            this.addKonvaObject(droppedItem, positionX/this.canvasContainer.scaleX(), positionY/this.canvasContainer.scaleY());
+            this.addKonvaObject(droppedItem, (positionX - this.canvasContainer.x()) / this.canvasContainer.scaleX() , (positionY - this.canvasContainer.y()) / this.canvasContainer.scaleY());
         }
     }
 
