d82fc68b03c7b0f9198f7af253c12f247bbcc11e
LucaLoubser <u20439963@tuks.co.za>
2023-07-14 00:22:44 +0200

    predefined email subjects as enum elements

---
+++ libs/api/email/util/src/enums/email-subjects.enum.ts
@@ -0,0 +1,7 @@
+export enum EmailSubject{
+    ACCEPT_VIEW_REQUEST= "Event Participation Trends: View Request Granted",
+    ACCEPT_MANAGER_REQUST= "Event Participation Trends: Manager Request Granted",
+    REJECT_VIEW_REQUEST= "Event Participation Trends: View Request Denied",
+    REJECT_MANAGER_REQUST= "Event Participation Trends: Manager Request Denied",
+    REVOKE_VIEW_ACCESS= "Event Participation Trends: Viewer Access Revoked"
+}
