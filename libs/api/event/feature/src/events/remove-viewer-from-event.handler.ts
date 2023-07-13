c75b435f6a627903cf05121d4588900d4706c5ad
LucaLoubser <u20439963@tuks.co.za>
2023-07-14 00:33:38 +0200

    added email funcitonality for when a viewer's view privilege is revoked

--- libs/api/event/feature/src/events/remove-viewer-from-event.handler.ts
+++ libs/api/event/feature/src/events/remove-viewer-from-event.handler.ts
@@ -2,12 +2,15 @@ import { RemoveViewerFromEventEvent } from '@event-participation-trends/api/even
 import { UserRepository} from '@event-participation-trends/api/user/data-access';
 import { EventRepository } from '@event-participation-trends/api/event/data-access';
 import { IEventHandler, EventsHandler } from '@nestjs/cqrs';
+import { EmailService } from '@event-participation-trends/api/email/feature'
+import { EmailContent, EmailSubject} from '@event-participation-trends/api/email/util';
 
 @EventsHandler(RemoveViewerFromEventEvent)
 export class RemoveViewerFromEventEventHandler implements IEventHandler<RemoveViewerFromEventEvent> {
   constructor(
     private readonly eventRepository: EventRepository,
     private readonly userRepository: UserRepository,
+    private readonly emailService: EmailService,
     ) {}
     
     async handle(event: RemoveViewerFromEventEvent) {
@@ -15,7 +18,17 @@ export class RemoveViewerFromEventEventHandler implements IEventHandler<RemoveVi
         
         const userDoc = await this.userRepository.getUser(event.event.userEmail|| "");
     
-        if(event.event.eventId != undefined)
+        if(event.event.eventId != undefined){
             await this.eventRepository.removeViewer(event.event.eventId,userDoc[0]._id);
+
+            const eventDoc = await this.eventRepository.getEventById(event.event.eventId);
+
+            //notify user via Email
+            this.emailService.sendEmail(
+                event.event.userEmail || "", 
+                EmailSubject.REVOKE_VIEW_ACCESS,
+                EmailContent.REVOKE_VIEW_ACCESS_CONTENT+ eventDoc[0].Name
+            )
+        }
     }
 }
