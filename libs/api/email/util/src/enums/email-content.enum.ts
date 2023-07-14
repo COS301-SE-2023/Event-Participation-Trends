46c1883eb9dc6857d1bb3f175f79107020388302
LucaLoubser <u20439963@tuks.co.za>
2023-07-14 18:31:03 +0200

    added email content for event creation email

--- libs/api/email/util/src/enums/email-content.enum.ts
+++ libs/api/email/util/src/enums/email-content.enum.ts
@@ -1,7 +1,8 @@
 export enum EmailContent{
-    ACCEPT_VIEW_REQUEST_CONTENT="You have been made a Viewer of the event: ",
-    ACCEPT_MANAGER_REQUEST_CONTENT="You have been made a Manager of the event: ",
-    REJECT_VIEW_REQUEST_CONTENT="You have been denied Viewer access to the event: ",
-    REJECT_MANAGER_REQUEST_CONTENT="You have been denied Manager access to the event: ",
-    REVOKE_VIEW_ACCESS_CONTENT="You have been revoked Viewer access to the event: ",
+    ACCEPT_VIEW_REQUEST_CONTENT= "You have been made a Viewer of the event: ",
+    ACCEPT_MANAGER_REQUEST_CONTENT= "You have been made a Manager of the event: ",
+    REJECT_VIEW_REQUEST_CONTENT= "You have been denied Viewer access to the event: ",
+    REJECT_MANAGER_REQUEST_CONTENT= "You have been denied Manager access to the event: ",
+    REVOKE_VIEW_ACCESS_CONTENT= "You have been revoked Viewer access to the event: ",
+    CREATE_EVENT_CONTENT= "Successfully created event: ",
 }
