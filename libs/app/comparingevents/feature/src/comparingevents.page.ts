eee3704a0ed9fd27d61fe7d28fce88ee31953f1e
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-07-15 12:48:28 +0200

    Updated refresher to refresh entire page

--- libs/app/comparingevents/feature/src/comparingevents.page.ts
+++ libs/app/comparingevents/feature/src/comparingevents.page.ts
@@ -32,6 +32,8 @@ export class ComparingeventsPage {
   searchSize = 0;
   categoryList: string[] = [];
   userRole: string | undefined;
+  didRefreshPage = false;
+  isLoading = true;
 
   constructor(
     private containerElement: ElementRef,
@@ -41,153 +43,57 @@ export class ComparingeventsPage {
 
   ngAfterViewInit() {
     this.checkOverflow();
-
-    // this.store.dispatch(new GetAllCategories());
-
-    // this.categories$.subscribe((categories) => {
-    //   if (categories) {
-    //     this.store.dispatch(new SetSelectedCategory(categories[0]));
-    //   }
-    // });
   }
 
   ngOnInit() {
     // if user role is admin, get all categories
-    this.store.dispatch(new GetRole());
+    setTimeout(() => {
+      this.store.dispatch(new GetRole());
 
-    this.role$.subscribe((role) => {
-      this.userRole = role ? role : '';
-      console.log('role: ', role);
+      this.role$.subscribe((role) => {
+        this.userRole = role ? role : '';
+        console.log('role: ', role);
 
-      if (role === 'admin') {
-        //get all categories
-        this.store.dispatch(new GetAllCategories());
+        if (role === 'admin') {
+          //get all categories
+          this.store.dispatch(new GetAllCategories());
 
-        this.categories$.subscribe((categories) => {
-          if (categories) {
-            this.categoryList = categories;
+          this.categories$.subscribe((categories) => {
+            if (categories) {
+              this.categoryList = categories;
 
-            //set selected category to first category
-            this.store.dispatch(new SetSelectedCategory(categories[0]));
-          }
-        });
-      } 
-      else if (role === 'manager') {
-        //get managed event categories
-        this.store.dispatch(new GetManagedEventCategories());
+              //set selected category to first category
+              this.store.dispatch(new SetSelectedCategory(categories[0]));
+            }
+          });
+        } 
+        else if (role === 'manager') {
+          //get managed event categories
+          this.store.dispatch(new GetManagedEventCategories());
 
-        this.managedEventCategories$.subscribe((categories) => {
-          if (categories) {
-            this.categoryList = categories;
+          this.managedEventCategories$.subscribe((categories) => {
+            if (categories) {
+              this.categoryList = categories;
 
-            //set selected category to first category
-            this.store.dispatch(new SetSelectedCategory(categories[0]));
-          }
-        });
-      }
-    });
+              //set selected category to first category
+              this.store.dispatch(new SetSelectedCategory(categories[0]));
+            }
+          });
+        }
+      });
+
+      this.isLoading = false;
+    }, 1000);
   }
 
   checkOverflow() {
     const container = this.containerElement.nativeElement;
     this.overflow = container.scrollHeight > container.clientHeight;
   }
-  
-  // define events array
-  events:Event[] = [
-    {
-      id: 1,
-      name: 'Event 1',
-      date: '2021-01-01',
-      location: 'Location 1',
-      description: 'Description 1',
-      selected: false,
-    },
-    {
-      id: 2,
-      name: 'Event 2',
-      date: '2021-01-02',
-      location: 'Location 2',
-      description: 'Description 2',
-      selected: false,
-    },
-    {
-      id: 3,
-      name: 'Event 3',
-      date: '2021-01-03',
-      location: 'Location 3',
-      description: 'Description 3',
-      selected: false,
-    },
-    {
-      id: 4,
-      name: 'Event 4',
-      date: '2021-01-04',
-      location: 'Location 4',
-      description: 'Description 4',
-      selected: false,
-    },
-    {
-      id: 5,
-      name: 'Event 5',
-      date: '2021-01-01',
-      location: 'Location 5',
-      description: 'Description 5',
-      selected: false,
-    },
-    {
-      id: 6,
-      name: 'Event 6',
-      date: '2021-01-02',
-      location: 'Location 6',
-      description: 'Description 6',
-      selected: false,
-    },
-    {
-      id: 7,
-      name: 'Event 7',
-      date: '2021-01-03',
-      location: 'Location 7',
-      description: 'Description 7',
-      selected: false,
-    },
-    {
-      id: 8,
-      name: 'Event 8',
-      date: '2021-01-04',
-      location: 'Location 8',
-      description: 'Description 8',
-      selected: false,
-    },
-  ];
-
-  // events: Event[] = [];
 
   maxSelectionReached = false;
 
-  isEmpty(): boolean {
-    return this.events.length === 0;
-  }
-
   toggleItemSelection(category: string) {
-    // if (this.selectedEvents.includes(event)) {
-    //   // Remove from selectedEvents
-    //   this.selectedEvents = this.selectedEvents.filter(
-    //     (selectedEvent) => selectedEvent !== event
-    //   );
-    //   event.selected = false;
-    // } else if (this.selectedEvents.length < this.maxSelectionAllowed) {
-    //   // Add to selectedEvents
-    //   event.selected = true;
-    //   this.selectedEvents.push(event);
-    // }
-
-    // if (this.selectedEvents.length < this.maxSelectionAllowed) {
-    //   this.maxSelectionReached = false;
-    // } else {
-    //   this.maxSelectionReached = true;
-    // }
-    // console.log(category);
     this.store.dispatch(new SetSelectedCategory(category));
   }
 
@@ -266,7 +172,7 @@ export class ComparingeventsPage {
       });
 
       this.store.dispatch(new GetAllCategories());
-      // this.store.dispatch(new GetMyEvents());
+      this.store.dispatch(new GetManagedEventCategories());
 
       event.target.complete();
     }, 2000);
