7a119f6c87980d1e84dde14ef39ab1cf3a4726e9
LucaLoubser <u20439963@tuks.co.za>
2023-07-24 22:28:47 +0200

    removed unused database service

--- libs/api/database/feature/src/database.module.ts
+++ libs/api/database/feature/src/database.module.ts
@@ -1,9 +1,8 @@
 import { Module } from "@nestjs/common";
-import { DatabaseService } from "./database.service";
 import { DatabaseConfigService } from "./database-configuration.service";
 
 @Module({
-    providers: [DatabaseService, DatabaseConfigService],
-    exports: [DatabaseService, DatabaseConfigService],
+    providers: [DatabaseConfigService],
+    exports: [DatabaseConfigService],
 })
 export class DatabaseModule{}
