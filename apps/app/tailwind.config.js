e4c00316c9b838d309b09748257a23aa618b373d
DieSeeKat <lukasanthonissen@gmail.com>
2023-07-26 22:01:43 +0200

    Remove daisyUI darkmode

--- apps/app/tailwind.config.js
+++ apps/app/tailwind.config.js
@@ -3,6 +3,9 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [],
+  },
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
