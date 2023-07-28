04f1686fdfd44ae5c8300777c934b8442351bb98
DieSeeKat <lukasanthonissen@gmail.com>
2023-07-28 16:48:48 +0200

    Add icon

--- libs/app/landing/src/lib/app-landing.module.ts
+++ libs/app/landing/src/lib/app-landing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
+import { NgIconsModule } from '@ng-icons/core';
+import { heroHome } from '@ng-icons/heroicons/outline';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, NgIconsModule.withIcons({heroHome})],
   declarations: [LandingComponent],
-  exports: [LandingComponent]
+  exports: [LandingComponent],
 })
 export class AppLandingModule {}
