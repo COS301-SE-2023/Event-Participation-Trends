1a0f01e26d0ddbb636fde115fa488f6d00a8e6c4
DieSeeKat <lukasanthonissen@gmail.com>
2023-07-28 22:28:02 +0200

    Add card fade

--- libs/app/components/src/lib/all-events-page/all-events-page.component.ts
+++ libs/app/components/src/lib/all-events-page/all-events-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OnInit } from '@angular/core';
+import { AppApiService } from '@event-participation-trends/app/api';
+import { IEvent } from '@event-participation-trends/api/event/util';
 
 @Component({
   selector: 'event-participation-trends-all-events-page',
@@ -8,4 +11,36 @@ import { CommonModule } from '@angular/common';
   templateUrl: './all-events-page.component.html',
   styleUrls: ['./all-events-page.component.css'],
 })
-export class AllEventsPageComponent {}
+export class AllEventsPageComponent implements OnInit {
+
+  constructor(private appApiService: AppApiService) {}
+
+  public all_events: IEvent[] = [];
+  public subscribed_events: IEvent[] = [];
+  public role = 'viewer';
+
+  async ngOnInit() {
+
+    this.role = await this.appApiService.getRole();
+
+    this.all_events = await this.appApiService.getAllEvents();
+
+    if (this.role !== 'admin') {
+      this.subscribed_events = await this.appApiService.getSubscribedEvents();
+    }
+
+  }
+
+  isAdmin() {
+    return this.role === 'admin';
+  }
+
+  isManager() {
+    return this.role === 'manager';
+  }
+
+  isViewer() {
+    return this.role === 'viewer';
+  }
+
+}
