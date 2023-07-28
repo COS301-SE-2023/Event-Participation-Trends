ad36d56cec3e8362f91ae3502af4fe80dc60c861
Stefan van der Merwe <u21429121@tuks.co.za>
2023-07-28 15:12:24 +0200

    Add bg follow cursor

--- libs/app/landing/src/lib/landing/landing.component.ts
+++ libs/app/landing/src/lib/landing/landing.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'event-participation-trends-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css'],
 })
-export class LandingComponent {}
+export class LandingComponent implements AfterViewInit {
+  @ViewChild('gradient') gradient!: ElementRef<HTMLDivElement>;
+  ngAfterViewInit() {
+    document.addEventListener("mousemove", (event) => {
+      const xPos = event.clientX / window.innerWidth;
+      const yPos = event.clientY / window.innerHeight;
+      this.gradient.nativeElement.style.backgroundImage = `radial-gradient(at ${xPos*100}% ${yPos*100}%, #22242C, #101010)`;
+    });
+    this.gradient.nativeElement.style.backgroundImage = `radial-gradient(at 50% 50%, #22242C, #101010)`;
+  }
+}
