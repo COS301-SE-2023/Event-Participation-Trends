54bcf2478fb92baffa70d82612328db0ba1586a4
DieSeeKat <lukasanthonissen@gmail.com>
2023-08-07 12:17:38 +0200

    Home chip nav

--- libs/app/home/src/lib/app-home.module.ts
+++ libs/app/home/src/lib/app-home.module.ts
@@ -4,10 +4,51 @@ import { RouterModule, Route } from '@angular/router';
 import { appHomeRoutes } from './lib.routes';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from '@event-participation-trends/app/components';
-import { AllEventsPageComponent, ComparePageComponent, UsersPageComponent } from '@event-participation-trends/app/components';
+import {
+  AllEventsPageComponent,
+  ComparePageComponent,
+  UsersPageComponent,
+} from '@event-participation-trends/app/components';
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  heroArrowLeft,
+  heroChartBar,
+  heroPencil,
+  heroArrowsRightLeft,
+} from '@ng-icons/heroicons/outline';
+import {
+  matFormatListBulletedRound,
+  matBarChartRound,
+  matDrawRound,
+  matQuestionMarkRound,
+  matGroupRound,
+  matEventRound,
+  matCompareArrowsRound,
+} from '@ng-icons/material-icons/round';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(appHomeRoutes), RouterModule, ProfileComponent, AllEventsPageComponent, ComparePageComponent, UsersPageComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(appHomeRoutes),
+    RouterModule,
+    ProfileComponent,
+    AllEventsPageComponent,
+    ComparePageComponent,
+    UsersPageComponent,
+    NgIconsModule.withIcons({
+      heroArrowLeft,
+      heroChartBar,
+      heroPencil,
+      matFormatListBulletedRound,
+      matBarChartRound,
+      matDrawRound,
+      matQuestionMarkRound,
+      matGroupRound,
+      matEventRound,
+      matCompareArrowsRound,
+      heroArrowsRightLeft
+    }),
+  ],
   declarations: [HomeComponent],
   bootstrap: [HomeComponent],
 })
