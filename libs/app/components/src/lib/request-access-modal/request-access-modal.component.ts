53d14160c5c0425a8766493d6bcee233a7f1afa4
Stefan van der Merwe <u21429121@tuks.co.za>
2023-08-08 16:53:02 +0200

    Add request access modal

---
+++ libs/app/components/src/lib/request-access-modal/request-access-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'event-participation-trends-request-access-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './request-access-modal.component.html',
+  styleUrls: ['./request-access-modal.component.css'],
+})
+export class RequestAccessModalComponent {}
