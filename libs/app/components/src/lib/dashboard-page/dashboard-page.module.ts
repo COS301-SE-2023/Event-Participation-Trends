14c147164342d7092daa0160add4a4fa2bcd3e01
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-08-09 11:34:22 +0200

    Added chart for new users detected

--- libs/app/components/src/lib/dashboard-page/dashboard-page.module.ts
+++ libs/app/components/src/lib/dashboard-page/dashboard-page.module.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { NgIconsModule } from "@ng-icons/core";
-import { heroUserGroup } from "@ng-icons/heroicons/outline";
+import { heroUserGroup, heroArrowTrendingUp } from "@ng-icons/heroicons/outline";
 import { DashboardPageComponent } from "./dashboard-page.component";
 
 @NgModule({
     bootstrap: [DashboardPageComponent],
     imports: [
         CommonModule, 
-        NgIconsModule.withIcons({heroUserGroup})
+        NgIconsModule.withIcons({heroUserGroup, heroArrowTrendingUp})
     ],
     declarations: [DashboardPageComponent]
 })
