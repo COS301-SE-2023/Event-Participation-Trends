d6ccd9e8298d6e4c75d3ec92fa725bc86053d303
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-08-09 13:11:52 +0200

    Fixed errors with nativeElement

--- libs/app/components/src/lib/dashboard-page/dashboard-page.component.ts
+++ libs/app/components/src/lib/dashboard-page/dashboard-page.component.ts
@@ -11,9 +11,6 @@ import { AppApiService } from '@event-participation-trends/app/api';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { IGetEventDevicePositionResponse, IGetEventFloorlayoutResponse, IGetEventResponse, IPosition } from '@event-participation-trends/api/event/util';
-import { Store } from '@ngxs/store';
-import { NgIconsModule } from '@ng-icons/core';
-import { heroUserGroup } from '@ng-icons/heroicons/outline';
 
 interface IAverageDataFound {
   id: number | null | undefined,
@@ -124,10 +121,371 @@ export class DashboardPageComponent implements OnInit {
 
     this.event = await this.appApiService.getEvent({ eventId: this.id });
 
+    const eventStartDate = this.event.StartDate;
+    const eventEndDate = this.event.EndDate;
+    
+    if (eventStartDate) {
+      this.eventStartTime = new Date(eventStartDate);
+    }
+    if (eventEndDate) {
+      this.eventEndTime = new Date(eventEndDate);
+    }
+
     this.loading = false;
     setTimeout(() => {
       this.show = true;
-    }, 200);
+    }, 200);    
+  }
 
+  dateToString(date: Date) {
+    return date.toString().replace(/( [A-Z]{3,4})$/, '').slice(0, 33);
+  }
+
+  async ngAfterViewInit() {
+    // wait until the heatmap container is rendered
+    setTimeout(() => {
+      this.isLoading = false;
+    
+      // set the number of hours of the event
+      let hoursOfEvent = 0;
+      if (this.eventStartTime.getHours() > this.eventEndTime.getHours()) {
+        hoursOfEvent = (24 - this.eventStartTime.getHours()) + this.eventEndTime.getHours();
+      } else {
+        hoursOfEvent = this.eventEndTime.getHours() - this.eventStartTime.getHours();
+      }
+      // set the labels of the x-axis
+      for (let i = 0; i <= hoursOfEvent; i++) {
+        let hours = '';
+        let minutes = '';
+        if (this.eventStartTime.getHours() + i < 10) {
+          hours = `0${this.eventStartTime.getHours() + i}`;
+        } 
+        else {
+          hours = `${this.eventStartTime.getHours() + i}`;
+        }
+
+        if (this.eventStartTime.getMinutes() < 10) {
+          minutes = `0${this.eventStartTime.getMinutes()}`;
+        }
+        else {
+          minutes = `${this.eventStartTime.getMinutes()}`;
+        }
+
+        this.eventHours.push(`${hours}:${minutes}`);
+      }
+
+      for (let i = 0; i < hoursOfEvent; i++) {
+        this.userDetectedPerHour.push(
+          {
+            time: this.eventHours[i],
+            detected: 0
+          }
+        )
+      }
+
+      // get average data points up to the current time to load heatmap
+    
+      setTimeout(() => {
+        Chart.register(ChartStreaming);
+        this.heatmap = new HeatMap({
+          container: document.getElementById('view')!,
+          maxOpacity: .6,
+          radius: 50,
+          blur: 0.90,
+        });
+        this.getImageFromJSONData(this.id);
+        // this.renderUserCountDataStreaming();
+        // this.renderTotalDevicesBarChart();
+      }, 1000);
+    }, 1000);    
+
+  //   const streamingInterval = setInterval(async () => {
+  //     const now = new Date();
+
+  //     if (now > this.eventEndTime) {
+  //       clearInterval(streamingInterval);
+  //     } else {
+
+  //       // //! Testing purposes
+
+  //       // now.setHours(now.getHours() - 11);
+  //       // now.setMinutes(now.getMinutes() - 20);
+
+  //       // get positions this interval
+
+  //       const intervalStart = new Date(now.getTime() - 5000);
+  //       const intervalEnd = now;
+
+  //       const positions = await this.appApiService.getEventDevicePosition(this.eventId, intervalStart, intervalEnd);
+
+  //       // add to heatmap
+
+  //       let data: {
+  //         x: number,
+  //         y: number,
+  //         value: number,
+  //         radius: number
+  //       }[] = [];
+
+  //       if (positions) {
+  //         data = positions.map((position: IPosition) => {
+  //           if (position.x != null && position.y != null) {
+  //             return {
+  //               x: position.x,
+  //               y: position.y,
+  //               value: 20,
+  //               radius: 10
+  //             };
+  //           } else {
+  //             return {
+  //               x: 100,
+  //               y: 100,
+  //               value: 0,
+  //               radius: 20
+  //             };
+  //           }
+  //         });
+
+  //         data.push({
+  //           x: 100,
+  //           y: 100,
+  //           value: 0,
+  //           radius: 20
+  //         });
+
+  //         this.heatmap?.setData({
+  //           max: 100,
+  //           min: 1,
+  //           data: data
+  //         });
+
+  //         this.heatmapData = data;
+
+  //         const unique_ids: number[] = [];
+  //         for (let i = 0; i < positions.length; i++) {
+  //           const position = positions[i];
+  //           if (position.id && !unique_ids.includes(position.id)) {
+  //             unique_ids.push(position.id);
+  //           }
+  //         }
+
+  //         const newData = unique_ids.length;
+  //         this.totalUsersDetected = newData;
+  //         const newTime = now.toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric", second: "numeric" });
+        
+  //         if (this.streamingUserCountChart) {
+  //           // add new label and data to the chart
+  //           this.streamingUserCountChart.data.labels?.push(newTime);
+  //           // add new data to the chart
+  //           this.streamingUserCountChart.data.datasets[0].data?.push(newData);
+  //           if (this.streamingUserCountChart.data.datasets[0].data?.length > 20) {
+  //             // remove first label
+  //             this.streamingUserCountChart.data.labels?.shift();
+  //             // remove first data point
+  //             this.streamingUserCountChart.data.datasets[0].data?.shift();
+  //           }
+  //           this.streamingUserCountChart.update();
+  //         }
+
+  //         // add new data to the streamingChartData
+  //         this.streamingChartData.labels.push(newTime);
+  //         this.streamingChartData.data.push(newData);
+      
+  //         // update the userDetectedPerHour array
+  //         this.userDetectedPerHour.forEach((hour) => {
+  //           // test if the hour is equal to the current hour
+  //           console.log(hour.time.slice(0, 2), newTime.slice(0, 2));
+  //           if (hour.time.slice(0, 2) === newTime.slice(0, 2)) {
+  //             // add one to the detected users
+  //             hour.detected = this.totalUsersDetected;
+  //           }
+  //         });
+  //         if (this.devicesBarChart) {
+  //           this.devicesBarChart.data.datasets[0].data = this.userDetectedPerHour.map((hour) => hour.detected);
+  //           this.devicesBarChart.data.labels = this.userDetectedPerHour.map((hour) => hour.time);
+  //           this.devicesBarChart.update();
+  //         }
+
+  //       }
+
+  //     }
+  //   }, 5000);
+  }
+
+  showToggleButton() {
+    this.showToggle = true;
+  }
+
+  hideToggleButton() {
+    this.showToggle = false;
+  }
+
+  removeArrowIconsFromGridTiles() {
+    for (let i = 0; i < this.gridTilesDataPoints.length; i++) {
+      const gridTile = this.gridTilesDataPoints[i].gridTile;
+      const arrowIcon = gridTile.children.item(0);
+      if (arrowIcon) {
+        gridTile.removeChild(arrowIcon);
+      }
+    }
+  }
+
+  toggleHeatmap() {
+    this.showHeatmap = !this.showHeatmap;
+  }
+
+  async getImageFromJSONData(eventId: string) {
+    const response = await this.appApiService.getEventFloorLayout(eventId);
+    if (response) {
+      // use the response to create an image
+      this.floorlayoutStage = new Konva.Stage({
+        container: 'floormap',
+        width: this.heatmapContainer.nativeElement.offsetWidth,
+        height: this.heatmapContainer.nativeElement.offsetHeight
+      });
+
+      // create node from JSON string
+      const layer: Konva.Layer = Konva.Node.create(response, 'floormap');
+      // add the node to the layer
+      this.floorlayoutStage.add(layer);
+    }
+  }
+
+  renderUserCountDataStreaming() {
+    const chartData: number[] = [];
+    const chartLabels: string[] = [];
+
+    this.streamingChartData.data.forEach((dataPoint) => {
+      chartData.push(dataPoint);
+    });
+
+    this.streamingChartData.labels.forEach((label, index) => {
+      chartLabels[index] = label;
+    });
+
+    const config: ChartConfiguration = {
+      type: 'line',             // 'line', 'bar', 'bubble' and 'scatter' types are supported
+      data: {
+        labels: chartLabels,
+        datasets: [{
+          data: chartData,
+        }]
+      },
+      options: {
+        plugins: {
+          tooltip: {
+            enabled: false
+          },
+          legend: {
+            display: false,
+          },
+          title: {
+            display: true,
+            text: 'Users Detected vs Time of Day (per 5 seconds)'
+          }
+        },
+        scales: {
+          x: {
+              display: true, 
+              title: {
+                display: true,
+                text: 'Time of day',
+              },
+            },
+            y: {
+              display: true,
+              title: {
+                display: true,
+                text: 'Users Detected', 
+              },
+              beginAtZero: true, 
+            },
+        }
+      }
+    };
+    const userCountDataStreamingCanvas = this.userCountDataStreamingChart.nativeElement;
+
+    if (userCountDataStreamingCanvas) {
+      const userCountDataStreamingCtx = userCountDataStreamingCanvas.getContext('2d', { willReadFrequently: true });
+      if (userCountDataStreamingCtx) {
+        this.streamingUserCountChart = new Chart(
+          userCountDataStreamingCtx, 
+          config        
+        );
+      }
+    }
+
+  }
+
+  renderTotalDevicesBarChart(){
+    const chartData: number[] = [];
+    const chartLabels: string[] = [];
+
+    this.userDetectedPerHour.forEach((timeData) => {
+      chartData.push(timeData.detected);
+      chartLabels.push(timeData.time);
+    });
+
+    const data = {
+        labels: chartLabels,
+        datasets: [{
+          data: chartData,
+          backgroundColor: [
+            'rgb(34 197 94)'
+          ],
+          borderColor: [
+            'rgb(34 197 94)'
+          ],
+          borderWidth: 1,
+        }]
+      };
+    
+    const config: ChartConfiguration = {
+      type: 'bar',
+      data: data,
+      options: {
+        plugins: {
+            title: {
+              display: true,
+              text: 'Users Detected vs Time of day (per hour)', 
+            },
+            legend:{
+                display: false
+            },
+            tooltip: {
+              enabled: false
+            }
+          },
+        scales: {
+          x: {
+            display: true, 
+              title: {
+              display: true,
+              text: 'Time of day',
+            },
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Number of Users Detected', 
+            },
+            beginAtZero: true, 
+          },
+        }
+      },
+    };
+    
+    const deviceBarChartCanvas = this.totalDevicesBarChart.nativeElement;
+
+    if (deviceBarChartCanvas) {
+      const deviceBarChartCtx = deviceBarChartCanvas.getContext('2d');
+      if (deviceBarChartCtx) {
+        this.devicesBarChart = new Chart(
+          deviceBarChartCanvas,
+          config
+        );
+      }
+    }
   }
 }
