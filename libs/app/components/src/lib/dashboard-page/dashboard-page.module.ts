9e2c7058d526b3eb008cd7616bf5accf26895b58
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-08-09 15:37:05 +0200

    Fixed arrow icons

--- libs/app/components/src/lib/dashboard-page/dashboard-page.module.ts
+++ libs/app/components/src/lib/dashboard-page/dashboard-page.module.ts
@@ -4,12 +4,13 @@ import { NgIconsModule } from "@ng-icons/core";
 import { heroUserGroupSolid } from "@ng-icons/heroicons/solid";
 import { heroBackward } from "@ng-icons/heroicons/outline";
 import { DashboardPageComponent } from "./dashboard-page.component";
+import { matKeyboardDoubleArrowUp, matKeyboardDoubleArrowDown } from "@ng-icons/material-icons/baseline";
 
 @NgModule({
     bootstrap: [DashboardPageComponent],
     imports: [
         CommonModule, 
-        NgIconsModule.withIcons({heroUserGroupSolid, heroBackward})
+        NgIconsModule.withIcons({heroUserGroupSolid, heroBackward, matKeyboardDoubleArrowUp, matKeyboardDoubleArrowDown})
     ],
     declarations: [DashboardPageComponent]
 })
