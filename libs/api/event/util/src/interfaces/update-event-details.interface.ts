fe6a05a4a242dbd3238e6c5c2df00a613fde2b70
LucaLoubser <u20439963@tuks.co.za>
2023-08-10 22:06:11 +0200

    Refactor Event Location: updated interfaces

--- libs/api/event/util/src/interfaces/update-event-details.interface.ts
+++ libs/api/event/util/src/interfaces/update-event-details.interface.ts
@@ -1,4 +1,3 @@
-import { IEventLocation } from "./event-location.interface";
 import { Types } from 'mongoose';
 
 export interface IUpdateEventDetails {
@@ -7,7 +6,7 @@ export interface IUpdateEventDetails {
     EndDate?: Date | undefined | null;
     Name?: string | undefined | null;
     Category?: string | undefined | null;
-    Location?: IEventLocation | undefined | null;
+    Location?: string | undefined | null;
     Manager?: Types.ObjectId | undefined | null;
     Floorlayout?: string | undefined | null;
 }
