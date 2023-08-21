91508e4c96cd957418f5beba85090890c9239f5d
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-08-21 12:53:43 +0200

    Added loading spinners

--- libs/app/components/src/lib/compare-page/compare-page.component.ts
+++ libs/app/components/src/lib/compare-page/compare-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppApiService } from '@event-participation-trends/app/api';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'event-participation-trends-compare-page',
@@ -8,4 +10,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './compare-page.component.html',
   styleUrls: ['./compare-page.component.css'],
 })
-export class ComparePageComponent {}
+export class ComparePageComponent {
+  public id = '';
+  public event : any | null = null;
+  public show = false;
+  public loading = true;
+  public categories: string[] = [];
+
+  constructor(private readonly appApiService: AppApiService, private readonly route: ActivatedRoute, private readonly router: Router) {}
+
+  async ngOnInit() {
+    // retrieve categories from API
+    this.categories = await this.appApiService.getManagedEventCategories();
+
+    this.loading = false;
+
+    setTimeout(() => {
+      this.show = true;
+    }, 200);    
+  }
+}
