278660dde3c270aa47cdb5a7cdc24936cd029359
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-08-21 13:16:55 +0200

    Added searchbar

--- libs/app/components/src/lib/compare-page/compare-page.component.ts
+++ libs/app/components/src/lib/compare-page/compare-page.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppApiService } from '@event-participation-trends/app/api';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'event-participation-trends-compare-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './compare-page.component.html',
   styleUrls: ['./compare-page.component.css'],
 })
@@ -16,17 +20,30 @@ export class ComparePageComponent {
   public show = false;
   public loading = true;
   public categories: string[] = [];
+  public show_search = true;
+  public role = 'viewer';
+  public search = '';
 
   constructor(private readonly appApiService: AppApiService, private readonly route: ActivatedRoute, private readonly router: Router) {}
 
   async ngOnInit() {
     // retrieve categories from API
-    this.categories = await this.appApiService.getManagedEventCategories();
+    this.role = await this.appApiService.getRole();
+
+    if (this.role === 'admin') {
+      //get all categories
+      this.categories = await this.appApiService.getAllEventCategories();
+    } else if (this.role === 'manager') {
+      //get managed categories
+      this.categories = await this.appApiService.getManagedEventCategories();
+    }
+
+    console.log(this.categories);
 
     this.loading = false;
 
     setTimeout(() => {
       this.show = true;
-    }, 200);    
+    }, 200);
   }
 }
