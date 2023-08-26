9f88946b72168365d34014cdc0b71bc92d450067
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-08-26 11:45:48 +0200

    Fixed scrolling overflow

--- libs/app/components/src/lib/compare-page/compare-page.module.ts
+++ libs/app/components/src/lib/compare-page/compare-page.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { NgIconsModule } from "@ng-icons/core";
 import { ComparePageComponent } from "./compare-page.component";
 import { matCheckCircleOutline } from "@ng-icons/material-icons/outline";
-import { matRadioButtonUnchecked, matSearch } from "@ng-icons/material-icons/baseline";
+import { matRadioButtonUnchecked, matSearch, matFilterCenterFocus, matZoomIn, matZoomOut } from "@ng-icons/material-icons/baseline";
 import { heroAdjustmentsHorizontal } from "@ng-icons/heroicons/outline";
 import { FormsModule } from "@angular/forms";
 
@@ -12,7 +12,7 @@ import { FormsModule } from "@angular/forms";
     imports: [
         CommonModule, 
         FormsModule,
-        NgIconsModule.withIcons({matCheckCircleOutline, matRadioButtonUnchecked, heroAdjustmentsHorizontal, matSearch})
+        NgIconsModule.withIcons({matCheckCircleOutline, matRadioButtonUnchecked, heroAdjustmentsHorizontal, matSearch, matFilterCenterFocus, matZoomIn, matZoomOut})
     ],
     declarations: [ComparePageComponent]
 })
