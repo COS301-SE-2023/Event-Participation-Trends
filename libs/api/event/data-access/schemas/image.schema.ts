ce1801c163dd2ee39655181a999ca34fe7df1da3
LucaLoubser <u20439963@tuks.co.za>
2023-09-03 13:53:40 +0200

    updated image scheman

--- libs/api/event/data-access/schemas/image.schema.ts
+++ libs/api/event/data-access/schemas/image.schema.ts
@@ -9,6 +9,12 @@ export class Image{
     @Prop({ type: String, required: true })
     imageBase64: string | undefined | null;
 
+    @Prop({ type: String, required: true })
+    imageType: string | undefined | null;
+
+    @Prop({ type: Number, required: true })
+    imageScale: number | undefined | null;
+
     @Prop({ type: mongoose.Schema.Types.ObjectId ,ref: 'Event', required: true })
     eventId: Types.ObjectId | undefined | null;
 }
