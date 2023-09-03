0acc907a63cae66af117aef0d8f84f40975ba87a
LucaLoubser <u20439963@tuks.co.za>
2023-09-03 13:56:27 +0200

    updated upload image request format

--- libs/api/event/util/src/requests/upload-image.request.ts
+++ libs/api/event/util/src/requests/upload-image.request.ts
@@ -2,4 +2,6 @@
 export interface IImageUploadRequest {
     eventId: string | undefined | null,
     floorlayoutImg: string | undefined | null;
+    imageScale: number | undefined | null;
+    imageType: string | undefined | null;
 }
