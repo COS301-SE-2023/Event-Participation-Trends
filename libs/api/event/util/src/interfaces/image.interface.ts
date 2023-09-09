0058a21f91b9cac7347e2556fc3261cd8b2142d9
LucaLoubser <u20439963@tuks.co.za>
2023-09-09 13:37:02 +0200

    updated image interface

--- libs/api/event/util/src/interfaces/image.interface.ts
+++ libs/api/event/util/src/interfaces/image.interface.ts
@@ -2,7 +2,9 @@ import { Types } from "mongoose";
 
 export interface IImage{
     imageBase64: string | undefined | null;
+    imageObj: string | undefined | null;
     eventId: Types.ObjectId | undefined | null;
     imageScale: number | undefined | null;
     imageType: string | undefined | null;
 }
+    
