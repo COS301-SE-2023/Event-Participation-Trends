2738ce0d34b3bc7bbdeb21920f8d7c3e31c13a34
LucaLoubser <u20439963@tuks.co.za>
2023-09-11 23:32:30 +0200

    added request format for updating floorlayout image details

---
+++ libs/api/event/util/src/requests/update-event-floorlayout-image.request.ts
@@ -0,0 +1,10 @@
+
+export interface IUpdateEventFloorLayoutImgRequest{
+    eventId: string | undefined | null;
+    imageId: string | undefined | null;
+    managerEmail: string | undefined | null;
+    imgBase64: string | undefined | null;
+    imageObj: string | undefined | null;
+    imageScale: number | undefined | null;
+    imageType: string | undefined | null;
+}
