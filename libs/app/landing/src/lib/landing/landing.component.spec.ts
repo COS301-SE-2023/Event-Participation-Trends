cae16e34aaffb1ead708ee96f311a7c3d0159cff
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-09-19 22:00:49 +0200

    Fixed default testing not working

--- libs/app/landing/src/lib/landing/landing.component.spec.ts
+++ libs/app/landing/src/lib/landing/landing.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LandingComponent } from './landing.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,6 +10,7 @@ describe('LandingComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LandingComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LandingComponent);
