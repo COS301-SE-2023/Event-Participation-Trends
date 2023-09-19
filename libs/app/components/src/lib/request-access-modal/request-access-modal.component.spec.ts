4c66803201103c098947378a10290b328ffc2ee7
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-09-19 18:29:09 +0200

    Added unit tests for request access modal

--- libs/app/components/src/lib/request-access-modal/request-access-modal.component.spec.ts
+++ libs/app/components/src/lib/request-access-modal/request-access-modal.component.spec.ts
@@ -19,4 +19,57 @@ describe('RequestAccessModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set the event id', () => {
+    component.setEventId('test');
+    expect(component.event_id).toEqual('test');
+  });
+
+  it('should get the button name', () => {
+    component.setEventId('test');
+    expect(component.getButtonName()).toEqual('request-access-test');
+  });
+
+  it('should add "hover:scale-90" class to target', () => {
+    const target = document.createElement('div');
+    target.id = 'test';
+    document.body.appendChild(target);
+
+    component.pressButton('#test');
+
+    expect(target.classList.contains('hover:scale-90')).toBeTruthy();
+  });
+
+  it('should remove "hover:scale-90" class from target', () => {
+    const target = document.createElement('div');
+    target.id = 'test';
+    target.classList.add('hover:scale-90');
+    document.body.appendChild(target);
+
+    component.pressButton('#test');
+
+    setTimeout(() => {
+      expect(target.classList.contains('hover:scale-90')).toBeFalsy();
+    }, 1000);
+  });
+
+  it('should get the request modal id', () => {
+    const event = { _id: 'test' };
+    expect(component.getRequestModalId(event)).toEqual('request-modal-test');
+  });
+
+  it('should close modal', () => {
+    const modal = document.createElement('div');
+    modal.id = 'profile-modal';
+    modal.classList.add('opacity-0');
+    modal.classList.add('hidden');
+    document.body.appendChild(modal);
+
+    component.closeModal();
+
+    expect(modal.classList.contains('opacity-0')).toBeTruthy();
+    expect(modal.classList.contains('hidden')).toBeTruthy();
+  });
+
+
 });
