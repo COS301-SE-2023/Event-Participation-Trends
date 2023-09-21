2648dfd9022e59adefb1d3903881e4fc57b8c8b6
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-09-21 23:06:14 +0200

    Fixed console.errors showing during tests

--- libs/app/landing/src/lib/landing/landing.component.spec.ts
+++ libs/app/landing/src/lib/landing/landing.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LandingComponent } from './landing.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ProfileComponent } from '@event-participation-trends/app/components';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -10,7 +11,7 @@ describe('LandingComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LandingComponent],
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule, ProfileComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LandingComponent);
