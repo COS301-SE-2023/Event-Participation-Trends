3fdf43fedf281dba8265c138a0faf214328026ce
LucaLoubser <u20439963@tuks.co.za>
2023-09-27 12:02:41 +0200

    updated chat message interface

--- libs/api/event/util/src/interfaces/chat-message.interface.ts
+++ libs/api/event/util/src/interfaces/chat-message.interface.ts
@@ -1,16 +1,22 @@
 
 export interface IChatMessage {
-    stallName?: string | undefined | null,
+    text: string | undefined | null,
     timestamp: Date | undefined | null,
-    message: string | undefined | null,
-    userEmail: string | undefined | null,
-    userProfileImageUrl: string | undefined | null,
-}
+    user: {
+        id:  string | undefined | null,
+        fullname:  string | undefined | null,
+        profilePic: string | undefined | null,
+        role: string | undefined | null,
+    },
+};
 
 export class ChatMessage {
-    stallName?: string | undefined | null;
+    text: string | undefined | null;
     timestamp: Date | undefined | null;
-    message: string | undefined | null;
-    userEmail: string | undefined | null;
-    userProfileImageUrl: string | undefined | null;
-}
+    user: {
+        id: string | undefined | null;
+        fullname: string | undefined | null;
+        profilePic: string | undefined | null;
+        role: string | undefined | null;
+    } | undefined | null;
+};
