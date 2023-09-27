f727af4df49a1d68cc72a93de088900ea325c59e
Stefan van der Merwe <u21429121@tuks.co.za>
2023-09-27 18:36:25 +0200

    Fix providers for test

--- apps/api/src/app/socket/socket.gateway.spec.ts
+++ apps/api/src/app/socket/socket.gateway.spec.ts
@@ -1,13 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SocketGateway } from './socket.gateway';
 import { SocketServiceService } from './socket-service.service';
+import { EventService } from '@event-participation-trends/api/event/feature';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 
 describe('SocketGateway', () => {
   let gateway: SocketGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SocketGateway, SocketServiceService],
+      providers: [SocketGateway, SocketServiceService, EventService, CommandBus, QueryBus],
     }).compile();
 
     gateway = module.get<SocketGateway>(SocketGateway);
